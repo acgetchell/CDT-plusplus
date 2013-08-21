3b654c8a1fec32b4d50041f9bdadbc5ecd55b0d7
Adam Getchell <acgetchell@ucdavis.edu>
2013-08-20 22:36:01 -0700

    Initial commit

---
+++ CDT-plusplus/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CDT-plusplus
+//
+//  Created by Getchell, Adam on 8/20/13.
+//  Copyright (c) 2013 UC Davis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
