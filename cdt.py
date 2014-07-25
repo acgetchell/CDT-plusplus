2998be28d9afa136b86307b3b0286d982db9d760
Adam Getchell <acgetchell@ucdavis.edu>
2014-07-25 16:41:00 -0700

    Docopt for python version!
    https://github.com/docopt/docopt

--- cdt.py
+++ cdt.py
@@ -1,12 +1,41 @@
-"""Causal Dynamical Triangulations in Python using CGAL
+#!/usr/bin/env python
 
-Copyright (c) 2013 Adam Getchell
+"""Causal Dynamical Triangulations in Python using CGAL.
 
-A program that generates spacetimes
+Copyright (c) 2014 Adam Getchell
+
+A program that generates d-dimensional triangulated spacetimes
+with a defined causal structure and evolves them according
+to the Metropolis algorithm.
 
 This is for quick and easy development in Python
-before writing in C++ for performance. """
+before writing in C++ for performance.
 
+Uses https://code.google.com/p/cgal-bindings/
+
+Usage:
+  cdt.py  ( --spherical | --toroidal | --periodic )
+          ( -n SIMPLICES)
+          ( -t TIMESLICES)
+          [ -d DIMENSIONS]
+  cdt.py --version
+  cdt.py --help
+
+Examples:
+  cdt.py --spherical -n 5000 -t 256
+  cdt-py --periodic -n 5000 -t 256 -d 3
+
+Options:
+  -h --help         Show this screen.
+  -v --version      Show version.
+  --spherical       Spherical topology
+  --toroidal        Toroidal topology
+  --periodic        Toroidal topology
+  -n SIMPLICES      Total number of simplices
+  -t TIMESLICES     Number of timeslices
+  -d DIMENSIONS     Dimensionality of triangulation [default: 3]
+
+"""
 
 from CGAL.CGAL_Kernel import Point_3
 from CGAL.CGAL_Triangulation_3 import Delaunay_triangulation_3
@@ -15,36 +44,25 @@ from CGAL.CGAL_Triangulation_3 import Delaunay_triangulation_3_Vertex_handle
 from CGAL.CGAL_Triangulation_3 import Ref_Locate_type_3
 from CGAL.CGAL_Triangulation_3 import VERTEX
 from CGAL.CGAL_Kernel import Ref_int
-from optparse import OptionParser
+
+from docopt import docopt
 import os
 import socket
 from datetime import datetime
 
-parser = OptionParser()
-parser.add_option("-d", "--dimension", dest="dimension",
-    help="Number of dimensions")
-parser.add_option("-f", "--file", dest="filename",
-    help="Open from data file")
-parser.add_option("-p", "--periodic", action="store_false", dest="topology",
-    help="Periodic (toroidal) topology")
-parser.add_option("-s", "--spherical", action="store_true", dest="topology",
-    help="Spherical topology")
-parser.add_option("-n", "--number-of-simplices", dest="simplices",
-    help="Number of simplices")
-parser.add_option("-t", "--timeslices", dest="timeslices",
-    help="Number of timeslices")
+if __name__ == '__main__':
+  arguments = docopt(__doc__, version='0.1')
+  # print arguments
 
-(options, args) = parser.parse_args()
+  print "Number of dimensions = ", arguments['-d']
+  print "Number of simplices = ", arguments['-n']
+  print "Number of timeslices = ", arguments['-t']
+  print "Topology is ", "spherical" if arguments['--spherical'] else "periodic"
+  print "User = ", os.getlogin()
+  print "Hostname = ", socket.gethostname()
+  print datetime.now().strftime("%Y-%m-%d.%X")
 
-print "Number of dimensions = ", options.dimension
-print "Number of simplices = ", options.simplices
-print "Number of timeslices = ", options.timeslices
-print "Topology is ", "spherical" if options.topology else "periodic"
-print "User = ", os.getlogin()
-print "Hostname = ", socket.gethostname()
-print datetime.now().strftime("%Y-%m-%d.%X")
-
-if options.topology:
-    print "Call make_S3_simplicial complex"
-else:
-    print "Call make_T3_simplicial complex"
+  if arguments['--spherical']:
+    print "Call make_S3_triangulation()"
+  else:
+    print "Call make_T3_triangulation()"
