82e5bacd977cdafdc5e285e20a06c864169f632a
Adam Getchell <acgetchell@ucdavis.edu>
2014-12-06 13:37:16 -0800

    Help message successfully parsed by docopt

--- src/cdt-docopt.cpp
+++ src/cdt-docopt.cpp
@@ -1,6 +1,7 @@
 #include "docopt/docopt.h"
 
 #include <iostream>
+#include <cstdlib>
 
 static const char USAGE[] =
 R"(Causal Dynamical Triangulations in C++ using CGAL.
@@ -11,27 +12,15 @@ A program that generates d-dimensional triangulated spacetimes
 with a defined causal structure and evolves them according
 to the Metropolis algorithm.
 
-Usage:
-./cdt  ( --spherical | --toroidal | --periodic )
-( -n SIMPLICES)
-( -t TIMESLICES)
-[ -d DIMENSIONS]
-./cdt --version
-./cdt --help
-
-Examples:
-./cdt --spherical -n 5000 -t 256
-./cdt --periodic -n 5000 -t 256 -d 3
+Usage:./cdt (--spherical | --toroidal) -n=SIMPLICES -t=TIMESLICES [-d=DIM] -k=K
 
 Options:
--h --help         Show this screen.
--v --version      Show version.
---spherical       Spherical topology
---toroidal        Toroidal topology
---periodic        Toroidal topology
--n SIMPLICES      Total number of simplices
--t TIMESLICES     Number of timeslices
--d DIMENSIONS     Dimensionality of triangulation [default: 3]
+  -h --help       Show this message
+  --version       Show program version
+  -n SIMPLICES    Approximate number of simplices
+  -t TIMESLICES   Number of timeslices
+  -d DIM          Dimensionality [default: 3]
+  -k K            K constant
 )";
 
 int main (int argc, char const *argv[])
@@ -39,11 +28,13 @@ int main (int argc, char const *argv[])
   std::map<std::string, docopt::value> args
     = docopt::docopt(USAGE,
                      { argv + 1, argv + argc},
-                     true,
-                     "CDT 1.0");
+                     true,          // print help message automatically
+                     "CDT 1.0");    // Version
 
+  // Debugging
   for (auto const& arg : args) {
     std::cout << arg.first << arg.second << std::endl;
   }
+
   return 0;
 }
