a5cf9774f4083731aba5c09cae7d037f76d8e860
Adam Getchell <acgetchell@ucdavis.edu>
2014-12-20 11:55:02 -0800

    Rewrote S3_bulk_action_alpha_minus_one to use same parameters

--- src/S3Action.h
+++ src/S3Action.h
@@ -18,63 +18,65 @@ typedef CGAL::Gmpzf Gmpzf;
 /// gravity in three dimensions.
 /// The formula is:
 /**
-\f[S^{(3)}(\alpha=-1)=-2\pi ikN_1+6N_3 ik\arccos\frac{1}{3}
-+\frac{iN_3\lambda}{6\sqrt(2)}=iS^{(3)}_{EDT}
+\f[S^{(3)}(\alpha=-1)=-2\pi ik N_1^{TL}+N_3^{(3,1)}\left(2.673ik+0.118i\lambda
+\right)+N_3^{(2,2)}\left(7.386ik+0.118i\lambda\right)\equiv iS^3_{EDT}
 \f]
-Where \f$N_1\f$ is the number of edges and \f$N_3\f$ is the number of simplices.
+Where \f$N_1^{TL}\f$ is the number of timelike links,
+\f$N_3^{(3,1)}\f$ is the number of (3,1) and (1,3) simplices, and
+\f$N_3^{(2,2)}\f$ is the number of (2,2) simplices.
 */
-Gmpzf S3_bulk_action_alpha_minus_one(unsigned N1,
-                                     unsigned N3,
-                                     long double K,
-                                     long double Lambda) {
+Gmpzf S3_bulk_action_alpha_minus_one(unsigned N1_TL,
+unsigned N3_31,
+unsigned N3_22,
+long double K,
+long double Lambda) {
   // Initialize for MPFR
-  mpfr_t n1, n3, k, lambda, two, pi, r1, r2, r3, six, r4, r5, one, three,
-         one_third, arc_cos, r6, square_root_two, r7, r8, r9, r10, total;
+  mpfr_t n1_tl, n3_31, n3_22, k, lambda, two, pi, r1, r2, r3, const2673,
+         const118, r4, r5, r6, r7, const7386, r8, r9, r10, r11, r12, total;
 
-  mpfr_inits(pi, r1, r2, r3, r4, r5, one_third, arc_cos, r6,
-             square_root_two, r7, r8, r9, r10, total, (mpfr_ptr) 0);
+  mpfr_inits(pi, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11,
+             r12, total, (mpfr_ptr) 0);
   // Set input parameters to mpfr_t equivalents
-  mpfr_init_set_ui(n1, N1, MPFR_RNDD);
-  mpfr_init_set_ui(n3, N3, MPFR_RNDD);
+  mpfr_init_set_ui(n1_tl, N1_TL, MPFR_RNDD);
+  mpfr_init_set_ui(n3_31, N3_31, MPFR_RNDD);
+  mpfr_init_set_ui(n3_22, N3_22, MPFR_RNDD);
   mpfr_init_set_ld(k, K, MPFR_RNDD);
   mpfr_init_set_ld(lambda, Lambda, MPFR_RNDD);
   mpfr_init_set_ld(two, 2.0, MPFR_RNDD);
   mpfr_const_pi(pi, MPFR_RNDD);
-  mpfr_init_set_ld(one, 1.0, MPFR_RNDD);
-  mpfr_init_set_ld(three, 3.0, MPFR_RNDD);
+  mpfr_init_set_ld(const2673, 2.673, MPFR_RNDD);
+  mpfr_init_set_ld(const118, 0.118, MPFR_RNDD);
+  mpfr_init_set_ld(const7386, 7.386, MPFR_RNDD);
 
   // First term accumulates in r3
-  mpfr_mul(r1, two, pi, MPFR_RNDD);  // 2*pi
-  mpfr_mul(r2, r1, k, MPFR_RNDD);  // 2*pi*k
-  mpfr_mul(r3, r2, n1, MPFR_RNDD);  // 2*pi*k*n1
+  mpfr_mul(r1, two, pi, MPFR_RNDD);  // r1 = 2*pi
+  mpfr_mul(r2, r1, k, MPFR_RNDD);  // r2 = 2*pi*k
+  mpfr_mul(r3, r2, n1_tl, MPFR_RNDD);  // r3 = 2*pi*k*n1_tl
 
-  // Second term accumulates in r6
-  mpfr_init_set_ld(six, 6.0, MPFR_RNDD);
-  mpfr_mul(r4, n3, six, MPFR_RNDD);  // 6*n3
-  mpfr_mul(r5, r4, k, MPFR_RNDD);  // 6*n3*k
-  mpfr_div(one_third, one, three, MPFR_RNDD);  // 1/3
-  mpfr_acos(arc_cos, one_third, MPFR_RNDD);  // arccos(1/3)
-  mpfr_mul(r6, r5, arc_cos, MPFR_RNDD);  // 6*n3*k*arccos(1/3)
+  // Second term accumulates in r7
+  mpfr_mul(r4, const2673, k, MPFR_RNDD); //  r4 = 2.673*k
+  mpfr_mul(r5, const118, lambda, MPFR_RNDD);  // r5 = 0.118*lambda
+  mpfr_add(r6, r4, r5, MPFR_RNDD);  // r6 = r4 + r5
+  mpfr_mul(r7, n3_31, r6, MPFR_RNDD);  // r7 = n3_31*r6
 
-  // Third term accumulates in r9
-  mpfr_sqrt(square_root_two, two, MPFR_RNDD);
-  mpfr_mul(r7, six, square_root_two, MPFR_RNDD);  // 6*sqrt(2)
-  mpfr_div(r8, lambda, r7, MPFR_RNDD);  // lambda/(6*sqrt(2))
-  mpfr_mul(r9, n3, r8, MPFR_RNDD);  // n3*lambda/(6*sqrt(2))
+  // Third term accumulates in r11
+  mpfr_mul(r8, const7386, k, MPFR_RNDD); // r8 = 7.386*k
+  mpfr_mul(r9, const118, lambda, MPFR_RNDD);  // r9 = 0.118*lambda
+  mpfr_add(r10, r8, r9, MPFR_RNDD);  // r10 = r8+r9
+  mpfr_mul(r11, n3_22, r10, MPFR_RNDD);  // r11 = n3_22*r10
 
-  // Result is r6+r9-r3
-  mpfr_add(r10, r6, r9, MPFR_RNDD);  // r6+r9
-  mpfr_sub(total, r10, r3, MPFR_RNDD);  // r6+r9-r3
-
-
-  // Debugging
-  // std::cout << "result is " << mpfr_out_str(stdout, 10, 0, total, MPFR_RNDD)
-  //                       << std::endl;
+  // Result is r3+r7+r11
+  mpfr_sub(r12, r7, r3, MPFR_RNDD);  // r12 = r7-r3
+  mpfr_add(total, r11, r12, MPFR_RNDD);  // total = r11+r12
 
   Gmpzf result = Gmpzf(mpfr_get_d(total, MPFR_RNDD));
 
-  mpfr_clears(n1, n3, k, lambda, two, pi, r1, r2, r3, six, r4, r5, one, three,
-              one_third, arc_cos, r6, square_root_two, r7, r8, r9, r10,
+  // Debugging
+  std::cout << "result is " << mpfr_out_str(stdout, 10, 0, total, MPFR_RNDD)
+  << std::endl;
+
+  mpfr_clears(n1_tl, n3_31, n3_22, k, lambda, two, pi, r1, r2, r3, const2673,
+              const118, r4, r5, r6, r7, const7386, r8, r9, r10, r11, r12,
               total, (mpfr_ptr) 0);
 
   return result;
--- unittests/S3BulkActionTest.cpp
+++ unittests/S3BulkActionTest.cpp
@@ -52,16 +52,25 @@ TEST_F(S3BulkAction, GetN1Values) {
 }
 
 TEST_F(S3BulkAction, CalculateAlphaMinus1BulkAction) {
-  unsigned N1 = static_cast<unsigned>(T.number_of_finite_edges());
-  std::cout << "(Unsigned) N1 = " << N1 << std::endl;
-  unsigned N3 = static_cast<unsigned>(T.number_of_finite_cells());
-  std::cout << "(Unsigned) N3 = " << N3 << std::endl;
+  unsigned N1_TL{0};
+  unsigned N1_SL{0};
+
+  classify_edges(&T, &N1_TL, &N1_SL);
+  std::cout << "(Unsigned) N1_TL = " << N1_TL << std::endl;
+
+  unsigned N3_31 = three_one.size() + one_three.size();
+  std::cout << "(Unsigned) N3_31 = " << N3_31 << std::endl;
+  unsigned N3_22 = two_two.size();
+  std::cout << "(Unsigned) N3_22 = " << N3_22 << std::endl;
+
   long double K = static_cast<long double>(1.1);
   std::cout << "(Long double) K = " << K << std::endl;
   long double Lambda = static_cast<long double>(2.2);
   std::cout << "(Long double) Lambda = " << Lambda << std::endl;
 
-  CGAL::Gmpzf Bulk_action = S3_bulk_action_alpha_minus_one(N1, N3, K, Lambda);
+  CGAL::Gmpzf Bulk_action = S3_bulk_action_alpha_minus_one(N1_TL,
+                                                           N3_31,
+                                                           N3_22, K, Lambda);
 
   EXPECT_THAT(Bulk_action, Ge(34000))  // Magic value from lots of tests
     << "Bulk action value wrong.";
