3952ef74eccaa5cd3e58e50c2948b3ac425f58cc
Adam Getchell <acgetchell@ucdavis.edu>
2015-04-21 15:25:13 -0700

    Starting move_26()

--- src/S3ErgodicMoves.h
+++ src/S3ErgodicMoves.h
@@ -134,51 +134,99 @@ void make_32_move(Delaunay* const D3,
 
 /// @brief Check a (2,6) move
 ///
-/// This function checks to see if a (2,6) move is possible.
+/// This function checks to see if a (2,6) move is possible. Starting with
+/// a (1,3) simplex, it checks all neighbors to see if there is a (3,1).
+/// If so, the index **n** of that neighbor is passed via an out parameter.
 ///
 /// @param[in,out] c The (1,3) simplex that is checked
 /// @param[out] n The integer value of the neighboring (3,1) simplex
+/// @returns True if the (2,6) move is possible
 auto is_26_movable(Cell_handle c, unsigned* n) noexcept {
   for (auto i = 0; i < 3; i++) {
     // Debugging
     std::cout << "Neighbor " << i << " is of type "
               << c->neighbor(i)->info() << std::endl;
     // Check all neighbors for a (3,1) simplex
-    if (c->neighbor(i)->info()==31) {
+    if (c->neighbor(i)->info() == 31) {
       *n = i;
       return true;
     }
   }
   // No neighboring (3,1) simplices found
   return false;
+}  // is_26_movable()
+
+/// @brief Convert (1,3) and (3,1) into 3 (1,3)s and 3(3,1)s
+///
+/// This function takes in a (1,3) and its neighboring (3,1) along with
+/// the index which labels their common face. The points of the common
+/// face are then averaged to get their center. A new vertex is inserted there.
+///
+/// @param[in,out] D3 The Delaunay triangulation
+/// @param[in,out] one_three_simplex The (1,3) simplex that will be deleted
+/// @param[in,out] neighbor_index Index labelling the (3,1) neighbor of the
+///                (1,3) simplex
+/// @param[in,out] three_one_simplex The (3,1) simplex that will be deleted
+void move_26(Delaunay* const D3,
+             Cell_handle one_three_simplex,
+             unsigned neighbor_index,
+             Cell_handle three_one_simplex) noexcept {
+  // First, verify cells are neighbors
+  assert(one_three_simplex.has_neighbor(three_one_simplex));
+
+  // Get vertices of common face
+  unsigned i1 = (neighbor_index + 1)&3;
+  std::cout << "Vertex index 1 is " << i1 << std::endl;
+  unsigned i2 = (neighbor_index + 2)&3;
+  std::cout << "Vertex index 2 is " << i2 << std::endl;
+  unsigned i3 = (neighbor_index + 3)&3;
+  std::cout << "Vertex index 3 is " << i3 << std::endl;
+
+  Vertex_handle v1 = one_three_simplex->vertex(i1);
+
+  // Average vertices to get new one in their center
+
+  // Assign a timeslice to the new vertex
+
+  // Hopefully the Delaunay re-triangulates correctly!
 }
+
 /// @brief Make a (2,6) move
 ///
 /// This function performs the (2,6) move by picking a (1,3) simplex
 /// and then checking that the spacelike face has an opposing (3,1)
-/// simplex. This is done using the is_26_movable() function.
+/// simplex. This is done using the **is_26_movable()** function, which in
+/// addition to returning a boolean value also has an out parameter which
+/// is the unsigned index of the first (3,1) neighbor that **is_26_movable()**
+/// finds. The actual move is performed by **move_26()**.
 ///
 /// @param[in,out]  D3 The Delaunay triangulation
 /// @param[in,out]  one_three  A list of (1,3) simplices to attempt move on
 void make_26_move(Delaunay* const D3,
                   std::vector<Cell_handle>* const one_three) noexcept {
   auto not_moved = true;
-  unsigned neighbor;
   while (not_moved) {
     // Pick a random simplex out of the one_three vector
     auto choice = generate_random_unsigned(0, one_three->size()-1);
+    unsigned neighboring_31_index;
     // Is there a neighboring (3,1) simplex?
-    if (is_26_movable((*one_three)[choice], &neighbor)) {
+    if (is_26_movable((*one_three)[choice], &neighboring_31_index)) {
       // Debugging
       std::cout << "(1,3) simplex " << choice << " is movable." << std::endl;
-      std::cout << "The neighboring simplex is of type "
-                << (*one_three)[choice]->neighbor(neighbor)->info()
+      std::cout << "The neighboring simplex " << neighboring_31_index
+                << " is of type "
+                << (*one_three)[choice]->neighbor(neighboring_31_index)->info()
                 << std::endl;
+      Cell_handle old_13_to_be_moved = (*one_three)[choice];
+      Cell_handle old_31_to_be_moved =
+        (*one_three)[choice]->neighbor(neighboring_31_index);
+      // Do the (2,6) move
+      move_26(D3, old_13_to_be_moved, neighboring_31_index, old_31_to_be_moved);
       not_moved = false;
     } else {
-      std::cout << "(1,3) simplex " << choice << " was not movable." << std::endl;
+      std::cout << "(1,3) simplex " << choice << " was not movable."
+                << std::endl;
     }
-
   }
 }  // make_26_move()
 
