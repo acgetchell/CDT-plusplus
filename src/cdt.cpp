6611db2817b32b999331fc5933754daae88ae488
Adam Getchell <acgetchell@ucdavis.edu>
2015-09-11 16:43:17 -0700

    Consistent variable names

--- src/cdt.cpp
+++ src/cdt.cpp
@@ -171,35 +171,6 @@ int main(int argc, char* const argv[]) {
   // The main work of the program
   // metropolis(universe, number_of_passes, output_every_n_passes);
 
-
-    // auto metropolis =
-    //   std::make_unique<decltype(universe)>(Metropolis(universe));
-    // std::unique_ptr<Metropolis> metropolis = std::make_unique<Metropolis>(universe);
-    // Metropolis metropolis(universe);
-
-  // // Main loop of program
-  // for (auto i = 0; i < passes; ++i) {
-  //   // Initialize data and data structures needed for ergodic moves
-  //   // each pass.
-  //   // make_23_move(&SphericalUniverse, &two_two) does the (2,3) move
-  //   // two_two is populated via classify_3_simplices()
-  //
-  //   // Get timelike edges V2 for make_32_move(&SphericalUniverse, &V2)
-  //   std::vector<Edge_tuple> V2;
-  //   auto N1_SL = static_cast<unsigned>(0);
-  //   get_timelike_edges(SphericalUniverse, &V2, &N1_SL);
-  //
-  //   auto moves_this_pass = SphericalUniverse.number_of_finite_cells();
-  //
-  //   std::cout << "Pass #" << i+1 << " is "
-  //             << moves_this_pass
-  //             << " attempted moves." << std::endl;
-  //
-  //   for (auto j = 0; j < moves_this_pass; ++j) {
-  //     // Metropolis algorithm to select moves goes here
-  //   }
-  // }
-
   // Output results
   t.stop();  // End running time counter
   std::cout << "Final Delaunay triangulation has ";
