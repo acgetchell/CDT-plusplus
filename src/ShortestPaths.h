fc02cf7ef546509092af841afd9c4743567eb3c8
gxn1710 <gnagar1996@gmail.com>
2016-05-21 00:38:43 -0500

    Added ShortestPaths.h

--- src/ShortestPaths.h
+++ src/ShortestPaths.h
@@ -2,75 +2,195 @@
 ///
 /// Copyright (c) 2015 Adam Getchell
 ///
-/// Performs the Metropolis-Hastings algorithm on the foliated Delaunay
-/// triangulations.
+/// Performs shortest paths algorithms on graphs using Bellman-Ford Algorithm and Voronoi Diagrams
 /// For details see:
-/// M. Creutz, and B. Freedman. “A Statistical Approach to Quantum Mechanics.”
-/// Annals of Physics 132 (1981): 427–62.
-/// http://thy.phy.bnl.gov/~creutz/mypubs/pub044.pdf
+/// ADD REFERENCES
+/// ADD REFERENCES
 
-/// \done Initialization
-/// \done operator()
-/// \done CalculateA1
-/// \done CalculateA2
-/// \done Update N1_TL_, N3_31_ and N3_22_ after successful moves
-/// \todo Atomic integral types for safe multithreading
-/// \todo Debug occasional infinite loops and segfaults!
-/// \todo Implement 3D Metropolis algorithm in operator()
-/// \todo Implement concurrency
 
-/// @file Metropolis.h
-/// @brief Perform Metropolis-Hastings algorithm on Delaunay Triangulations
-/// @author Adam Getchell
-/// @bug <a href="http://clang-analyzer.llvm.org/scan-build.html">
-/// scan-build</a>: No bugs found.
+/// \todo Graph Initialization
+/// \todo AddVertex()
+/// \todo AddEdge()
+/// \todo IsNegativeCycle()
+/// \todo CalculateBellmanFord()
+/// \todo CalculateVoronoi()
 
-#ifndef SRC_METROPOLIS_H_
-#define SRC_METROPOLIS_H_
+/// @file ShortestPaths.h
+/// @brief Calculate shortest path of a graph using Bellman Ford Algorithm and Voronoi Diagrams
+/// @author Gaurav Nagar
 
-// CGAL headers
-// #include <CGAL/Gmpzf.h>
-// #include <CGAL/Gmpz.h>
-// #include <mpfr.h>
-// #include <CGAL/Mpzf.h>
 
-// CDT headers
-#include "S3ErgodicMoves.h"
-#include "S3Action.h"
+#ifndef SRC_SHORTESTPATHS_H_
+#define SRC_SHORTESTPATHS_H_
+
+
 
 // C++ headers
+#include <limits>
 #include <tuple>
-// #include <atomic>
 #include <vector>
-#include <utility>
+#include <map>
 #include <algorithm>
 
-using Gmpzf = CGAL::Gmpzf;
+const double inf = std::numerical_limits<double>::infinity(); //constant used to signify no edge between two nodes
 
-extern const std::uintmax_t PRECISION;
+class Vertex{
 
-enum class move_type {TWO_THREE = 0,
-                      THREE_TWO = 1,
-                      TWO_SIX = 2,
-                      SIX_TWO = 3,
-                      FOUR_FOUR = 4};
+    private:
+        double x_, y_, z_;
+        double sourceDistance_;
+        int prev_; //index of previous vertex
 
-/// @class Metropolis
+    public:
+        Vertex(){
+            Vertex(0, 0, 0);
+        }
+
+        Vertex(double x, double y, double z){
+            x_ = x;
+            y_ = y;
+            z_ = z;
+            sourceDistance_ = std::numerical_limits<double>::infinity();
+        }
+
+        double getDistance(){
+            return sourceDistance_;
+        }
+
+        int getPrev(){
+            return prev_;
+        }
+
+        void setDistance(double distance){
+            sourceDistance_ = distance;
+        }
+
+        void setPrev(int prevVertex){
+            prev_ = prevVertex;
+        }
+};
+
+class Edge{
+
+    private:
+        Vertex u_, v_; //indices of vertex u, v
+        double weight_; //weight of the edge
+
+    public:
+        Edge(){
+            Edge(0, 1, 0.0);
+        }
+
+        Edge(Vertex u, Vertex v, double weight){
+            u_ = u;
+            v_ = v;
+            weight_ = weight;
+        }
+
+        Vertex getU(){
+            return u_;
+        }
+
+        Vertex getV(){
+            return v_;
+        }
+
+        double getWeight(){
+            return weight_;
+        }
+
+        void setU(Vertex u){
+            u_ = u;
+        }
+
+        void setV(Vertex v){
+            v_ = u;
+        }
+
+        void setWeight(double weight){
+            weight_ = weight;
+        }
+};
+
+/// @class Graph
 ///
-/// @brief Metropolis-Hastings algorithm functor
+/// @brief Graph class that provides shortest paths functionalities
 ///
-/// The Metropolis-Hastings algorithm is a Markov Chain Monte Carlo method.
-/// The probability of making an ergodic (Pachner) move is:
+/// The Bellman-Ford algorithm is a method that calculates the shortest paths on graphs with even negative weights.
+/// The reccurrence for the shortest paths is:
 ///
-/// \f[P_{ergodic move}=a_{1}a_{2}\f]
-/// \f[a_1=\frac{move[i]}{\sum\limits_{i}move[i]}\f]
-/// \f[a_2=e^{\Delta S}\f]
-class Metropolis {
+/// \D[i, j] = 0                                       if i = t,  j = 0
+///            inf                                     if i != t, j = 0
+///            min{D[k, j - 1] + w[i, k], D[i, j - 1]} if (i, k) is an edge
+
+class Graph{
+
+    private:
+        vector<Vertex> vertices_;
+        int numVertices_;
+        int numEdges_;
+        vector<Edge> edges_;
+        vector<vector<double>> adjMatrix_;
+        vector<Vertex> path_;
+        map<Vertex:int> verticesMap_;
+
+    public:
+        Graph(vector<Vertex> vertices, vector<Edge> edges){
+            vertices_ = vertices;
+            edges_ = edges;
+            numVertices_ = vertices.size();
+            numEdges_ = edge.size();
+            for(int i = 0; i < numVertices_; i++){
+                map[vertices_.at(i)] = i;
+            }
+            computeAdjMatrix();
+        }
+
+        void computeAdjMatrix(){
+            for (int i = 0; i < numVertices_; i++){
+                vertex<int> adjList; //adjList for ith vertex
+                for (int j = 0; j < numVertices_; j++){
+                    adjList.push_back(inf);
+                }
+                adjMatrix_.push_back(adjList);
+            }
+
+            for (int i = 0; i < numEdges_; i++){
+                int uIndex = verticesMap_[edges_.at(i).getU()];
+                int vIndex = verticesMap_[edges_.at(i).getV()];
+                adjMatrix_.at(uIndex).at(vIndex) = edges_.at(i).getWeight(); //edge weight between vertices u and v
+                adjMatrix_.at(vIndex).at(uIndex) = edges_.at(i).getWeight();
+            }
+        }
+
+        bool isNegativeCycle(){
+            return true;
+        }
+
+        void calculateBellmanFord(Vertex s, Vertex t){
+            for(int i = 0; i < numVertices_; i++){
+                for(int j = 0; j < numVertices_; j++){
+                    for(int k = 0; k < numVertices_; k++){
+
+                    }
+                }
+            }
+        }
+
+        double calculateOptimalPathCost(Vertex s, Vertex t){
+            return 0;
+        }
+
+        vector<Vertex> calculateOptimalPath(Vertex s, Vertex t){
+            return path_;
+        }
+};
+
+/*
+class Metropolis { 
  public:
-  /// @brief Metropolis constructor
+  /// @brief Graph constructor
   ///
-  /// Minimal setup of runtime job parameters.
-  /// All the real work is done by operator().
   ///
   /// @param[in] Alpha  \f$\alpha\f$ is the timelike edge length
   /// @param[in] K      \f$k=\frac{1}{8\pi G_{Newton}}\f$
@@ -93,6 +213,7 @@ class Metropolis {
     #endif
   }
 
+  Graph(unsigned int nodes)
   /// Gets value of **Alpha_**.
   auto Alpha() const noexcept {return Alpha_;}
 
@@ -649,5 +770,7 @@ class Metropolis {
   std::pair<std::vector<Edge_tuple>, std::uintmax_t> movable_edge_types_;
   ///< Movable timelike and spacelike edges.
 };  // Metropolis
+*/
 
-#endif  // SRC_METROPOLIS_H_
+
+#endif  // SRC_SHORTESTPATHS_H_
