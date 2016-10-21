73bf6a731965d68affc582e02338abc5672bb8c0
Adam Getchell <acgetchell@ucdavis.edu>
2016-10-21 00:07:23 -0700

    (Ab)using lambdas for nicer workflow
    cdt-opt compiles but doesn’t run. Track down segfault there.

--- src/cdt-opt.cpp
+++ src/cdt-opt.cpp
@@ -1,6 +1,20 @@
-//
-// Created by Adam Getchell on 2016-10-18.
-//
+/// Causal Dynamical Triangulations in C++ using CGAL
+///
+/// Copyright (c) 2016 Adam Getchell
+///
+/// Full run-through with default options used to calculate
+/// optimal values for thermalization, etc. A simpler version
+/// that encompasses the entire lifecycle. Also suitable for
+/// scripting parallel runs.
+///
+/// Inspired by http://cppcon.org/modernizing-your-c/
+///
+/// \todo Invoke Metropolis algorithm
+/// \todo Print out graph of time-value vs. volume vs. pass number
+
+/// @file cdt-opt.cpp
+/// @brief Outputs values to determine optimizations
+/// @author Adam Getchell
 
 #include <functional>
 #include <iostream>
@@ -30,8 +44,8 @@ struct Simulation {
 
 int main() {
   std::cout << "cdt-opt running ..." << std::endl;
-  constexpr int         simplices  = 640000;
-  constexpr int         timeslices = 256;
+  constexpr int         simplices  = 64000;
+  constexpr int         timeslices = 25;
   constexpr long double alpha      = 1.1;
   constexpr long double k          = 2.2;
   constexpr long double lambda     = 3.3;
@@ -50,7 +64,9 @@ int main() {
   // universe = my_algorithm.start(universe);
 
   // Here's the desired interface
-  //  my_simulation.queue(my_algorithm(universe));
+//  my_simulation.queue(my_algorithm(universe));
+  // \todo Fix segfault
+  my_simulation.queue([&](SimplicialManifold s) { return my_algorithm(s);});
   // my_simulation.queue(EuclideanDeSitter())
   // my_simulation.queue(print_results())
   universe = my_simulation.start(universe);
