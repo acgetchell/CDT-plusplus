5dbf6476190d9187c7e5282edbd537f5f47f9666
Adam Getchell <acgetchell@ucdavis.edu>
2016-12-26 22:28:53 -0800

    MoveManager test fixed and working
    We’ll see what gcc says

--- unittests/MoveManagerTest.cpp
+++ unittests/MoveManagerTest.cpp
@@ -277,7 +277,6 @@ TEST_F(MoveManagerTest, OptionTypesTest) {
       << "attempted_moves_ (4,4) move count wrong.";
 }
 
-// \todo: Fix MoveManager tests
 TEST_F(MoveManagerTest, MakeA23Move) {
   EXPECT_TRUE(universe_.triangulation->tds().is_valid())
       << "MoveManagerTest constructed member universe_ is invalid.";
@@ -314,19 +313,23 @@ TEST_F(MoveManagerTest, MakeA23Move) {
 
   //  this_move.operator()();
   universe_ = *this_move.operator()();
+  attempted_moves_ = *this_move.attempted_moves_;
 
   std::cout << "this_move has " << this_move.universe_.get().geometry->N3_22()
             << " (2,2) cells." << std::endl;
-
-  std::cout << "this_move has " << this_move.universe_.get().geometry->N3_31()
-            << " (1,3) and (3,1) cells." << std::endl;
 
   std::cout << "maybe_moved_universe has "
             << maybe_moved_universe.get().geometry->N3_22() << " (2,2) cells."
             << std::endl;
 
+  std::cout << "universe_ has "
+            << universe_.geometry->N3_22() << " (2,2) cells." << std::endl;
+
   EXPECT_TRUE(this_move.universe_.get().triangulation->tds().is_valid())
       << "this_move.universe.triangulation invalid.";
+
+  EXPECT_TRUE(universe_.triangulation.get()->tds().is_valid())
+      << "MoveManager's universe_.triangulation invalid";
 
 //  EXPECT_TRUE(maybe_moved_universe.get().triangulation->tds().is_valid())
 //      << "maybe_moved_universe.triangulation invalid.";
@@ -336,185 +339,21 @@ TEST_F(MoveManagerTest, MakeA23Move) {
   EXPECT_TRUE(N3_22_before == this_move.universe_.get().geometry->N3_22() - 1)
       << "MoveManager didn't add a (2,2) simplex.";
 
+  EXPECT_TRUE(N3_22_before == universe_.geometry->N3_22() - 1)
+      << "MoveManager didn't add a (2,2) simplex.";
+
   EXPECT_FALSE(universe_.triangulation == nullptr)
       << "MoveManagerTest member universe_ is a null pointer after move.";
 
   std::cout << std::get<0>(this_move.attempted_moves_.get())
             << " (2,3) moves attempted." << std::endl;
-  EXPECT_THAT(std::get<0>(this_move.attempted_moves_.get()),
-              Gt(std::get<0>(attempted_moves_)))
+
+  std::cout << "MoveManagerTest member attempted_moves_ is "
+            << std::get<0>(attempted_moves_) << std::endl;
+
+  EXPECT_THAT(std::get<0>(this_move.attempted_moves_.get()), Gt(0))
+      << "Move manager didn't attempt a (2,3) move.";
+
+  EXPECT_THAT(std::get<0>(attempted_moves_), Gt(0))
       << "Move manager didn't attempt a (2,3) move.";
 }
-
-// TEST_F(MoveManagerTest, MakeA23Move) {
-//  EXPECT_TRUE(universe_.triangulation->tds().is_valid())
-//      << "Constructed universe_ is invalid.";
-//
-//  auto move = MoveManager<SimplicialManifold, Move_tuple>(
-//      std::move(universe_), std::move(attempted_moves_));
-//  auto move = MoveManager<boost::optional<SimplicialManifold>,
-//                          boost::optional<Move_tuple>>(
-//      std::move(maybe_moved_universe), std::move(maybe_move_count));
-
-//  move(true);
-//
-//  if (move.universe_) {
-//    universe_ = std::move(move.universe_.get());
-//    attempted_moves_ = std::move(move.attempted_moves_.get());
-//  }
-//  universe_ = std::move(move.universe_);
-
-//  EXPECT_TRUE(universe_.triangulation->tds().is_valid())
-//      << "Universe invalid after the move.";
-//}
-// TEST_F(MoveManagerTest, MakeA23MoveOnACopyAndSwap) {
-//  EXPECT_TRUE(this->universe_.triangulation->tds().is_valid())
-//      << "Constructed universe_ is invalid.";
-//
-//  // Make a copy using Delaunay copy-ctor
-//  //  auto tempDT     = Delaunay(*(this->universe_.triangulation));
-//  //  auto tempDT_ptr = std::make_unique<Delaunay>(tempDT);
-//  SimplicialManifold copied_manifold{this->universe_};
-//
-//  EXPECT_TRUE(this->universe_.triangulation != copied_manifold.triangulation)
-//      << "Pointers are equal and/or point to the same location.";
-//
-//  //  auto tempSM = SimplicialManifold(std::move(tempDT_ptr));
-//
-//  EXPECT_TRUE(copied_manifold.triangulation->tds().is_valid())
-//      << "SimplicialManifold copy is invalid.";
-//
-//  //  tempSM = std::move(make_23_move(std::move(tempSM), attempted_moves_));
-//  copied_manifold =
-//      std::move(make_23_move(std::move(copied_manifold), attempted_moves_));
-//
-//  std::cout << "Attempted (2,3) moves = " << std::get<0>(attempted_moves_)
-//            << '\n';
-//
-//  EXPECT_TRUE(copied_manifold.triangulation->tds().is_valid())
-//      << "SimplicialManifold copy invalid after make_23_move().";
-//
-//  // Define swap for SimplicialManifold so that geometry is recalculated
-//  // when the triangulation is swapped
-//  //  this->universe_.swap(copied_manifold);
-//  //  boost::swap(this->universe_, copied_manifold);
-//  swap(this->universe_, copied_manifold);
-//
-//  EXPECT_TRUE(this->universe_.triangulation->tds().is_valid())
-//      << "universe_ invalid after swap with copied universe.";
-//
-//  // Print new values
-//  std::cout << "New values:\n";
-//  std::cout << "(3,1) simplices: " <<
-//  this->universe_.geometry->three_one.size()
-//            << "\n";
-//  std::cout << "(2,2) simplices: " << this->universe_.geometry->two_two.size()
-//            << "\n";
-//  std::cout << "(1,3) simplices: " <<
-//  this->universe_.geometry->one_three.size()
-//            << "\n";
-//  std::cout << "Timelike edges: "
-//            << this->universe_.geometry->timelike_edges.size() << "\n";
-//  std::cout << "Spacelike edges: "
-//            << this->universe_.geometry->spacelike_edges.size() << "\n";
-//  std::cout << "Vertices: " << this->universe_.geometry->vertices.size()
-//            << "\n";
-//
-//  EXPECT_THAT(std::get<0>(attempted_moves_), Ge(1))
-//      << "make_23_move() didn't record an attempted move.";
-//
-//  EXPECT_THAT(this->universe_.geometry->two_two.size(), Eq(N3_22_before + 1))
-//      << "make_23_move() didn't add one and only one (2,2) simplex.";
-//
-//  EXPECT_THAT(this->universe_.geometry->three_one.size(), Eq(N3_31_before))
-//      << "make_23_move() changed (3,1) simplices.";
-//
-//  EXPECT_THAT(this->universe_.geometry->one_three.size(), Eq(N3_13_before))
-//      << "make_23_move() changed (1,3) simplices.";
-//
-//  EXPECT_THAT(this->universe_.geometry->timelike_edges.size(),
-//              Eq(timelike_edges_before + 1))
-//      << "make_23_move() didn't add one and only one timelike edge.";
-//
-//  EXPECT_THAT(this->universe_.geometry->spacelike_edges.size(),
-//              Eq(spacelike_edges_before))
-//      << "make_23_move() changed the number of spacelike edges.";
-//
-//  EXPECT_THAT(this->universe_.geometry->vertices.size(), Eq(vertices_before))
-//      << "make_23_move() changed the number of vertices.";
-//}
-//
-// TEST_F(MoveManagerTest, DISABLED_MakeA23MoveManager) {
-//    EXPECT_TRUE(this->universe_->tds().is_valid())
-//                        << "Constructed universe_ is invalid.";
-//
-//    PachnerMove p(universe_,
-//                  move_type::TWO_THREE,
-//                  movable_simplex_types_,
-//                  movable_edge_types_);
-//
-//    std::cout << "Attempted (2,3) moves = " << std::get<0>(p.attempted_moves_)
-//    << '\n';
-//
-//    // Move info from MoveManager
-//    universe_ = std::move(p.universe_);
-//    std::get<0>(attempted_moves_) += std::get<0>(p.attempted_moves_);
-//
-//    EXPECT_TRUE(this->universe_->tds().is_valid())
-//                        << "MoveManager(TWO_THREE) invalidated universe_.";
-//
-//    // Re-populate with current data
-//    auto new_movable_simplex_types = classify_simplices(this->universe_);
-//    auto new_movable_edge_types = classify_edges(this->universe_);
-//
-//    // Print new values
-//    std::cout << "New values: " << '\n';
-//    std::cout << "(3,1) simplices: "
-//    << std::get<0>(new_movable_simplex_types).size() << '\n';
-//    std::cout << "(2,2) simplices: "
-//    << std::get<1>(new_movable_simplex_types).size() << '\n';
-//    std::cout << "(1,3) simplices: "
-//    << std::get<2>(new_movable_simplex_types).size() << '\n';
-//    std::cout << "Timelike edges: "
-//    << new_movable_edge_types.first.size() << '\n';
-//    std::cout << "Spacelike edges: "
-//    << new_movable_edge_types.second << '\n';
-//    std::cout << "Vertices: "
-//    << this->universe_->number_of_vertices() << '\n';
-//
-//    EXPECT_THAT(std::get<0>(attempted_moves_), Ge(1))
-//                        <<
-//                        "MoveManager(TWO_THREE) didn't record an attempted
-//                        move.";
-//
-//    EXPECT_THAT(std::get<1>(new_movable_simplex_types).size(),
-//                Eq(std::get<1>(movable_simplex_types_).size() + 1))
-//                        <<
-//                        "MoveManager(TWO_THREE) didn't add one and only one
-//                        (2,2) simplex.";
-//
-//    EXPECT_THAT(std::get<0>(new_movable_simplex_types).size(),
-//                Eq(std::get<0>(movable_simplex_types_).size()))
-//                        << "MoveManager(TWO_THREE) changed (3,1) simplices.";
-//
-//    EXPECT_THAT(std::get<2>(new_movable_simplex_types).size(),
-//                Eq(std::get<2>(movable_simplex_types_).size()))
-//                        << "MoveManager(TWO_THREE) changed (1,3) simplices.";
-//
-//    EXPECT_THAT(new_movable_edge_types.first.size(),
-//                Eq(movable_edge_types_.first.size() + 1))
-//                        <<
-//                        "MoveManager(TWO_THREE) didn't add one and only one
-//                        timelike edge.";
-//
-//    EXPECT_THAT(new_movable_edge_types.second, Eq(movable_edge_types_.second))
-//                        <<
-//                        "MoveManager(TWO_THREE) changed the number of
-//                        spacelike edges.";
-//
-//    EXPECT_THAT(this->universe_->number_of_vertices(),
-//                Eq(number_of_vertices_))
-//                        <<
-//                        "MoveManager(TWO_THREE) changed the number of
-//                        vertices.";
-//}
