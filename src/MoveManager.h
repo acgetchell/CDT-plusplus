1688499b80a90c16c1b428597de2a63316a94ffe
Adam Getchell <acgetchell@ucdavis.edu>
2017-02-28 11:45:11 -0800

    Fixup .travis.yml to use correct  compiler/abi/boost
    Adapted from Louis Dionne's .travis.yml for boost::hana
    
    https://github.com/boostorg/hana/blob/master/.travis.yml

--- src/MoveManager.h
+++ src/MoveManager.h
@@ -171,7 +171,7 @@ class MoveManager {
     // Disengage boost::optional value which returns results of invalid move
     // Only works on recent versions of Boost (>1.58)
     // This is the correct line for Metropolis() to work
-//    universe_ = {};
+    universe_ = {};
     // This line is just for testing until we properly disengage option-type
     return universe_;
   }
