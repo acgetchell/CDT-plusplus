b233b8a1d77466a0361633b6f301279e7a4e2b05
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-03-07 13:07:10 -0800

    Remove unneeded `virtual` on destructors of non-polymorphic classes.
    These classes can't be polymorphic, because they have copy/move
    constructors and assignment operators. Assignment doesn't make sense
    in a classical OOP world, because slicing.

--- src/SimplicialManifold.h
+++ src/SimplicialManifold.h
@@ -79,7 +79,7 @@ struct GeometryInfo {
         vertices{std::get<5>(geometry)} {}
 
   /// @brief Default destructor
-  virtual ~GeometryInfo() = default;
+  ~GeometryInfo() = default;
 
   /// @brief Default move constructor
   /// @return A GeometryInfo{}
@@ -197,7 +197,7 @@ struct SimplicialManifold {
             classify_all_simplices(triangulation))} {}
 
   /// @brief Destructor
-  virtual ~SimplicialManifold() {
+  ~SimplicialManifold() {
 #ifndef NDEBUG
     std::cout << "SimplicialManifold dtor." << std::endl;
 #endif
