1ebd9b2d68e92b6078a4a9af9c4c58b589d905e9
Adam Getchell <acgetchell@ucdavis.edu>
2017-03-16 17:05:28 -0700

    Now that CGAL isn't overriding CMAKE, shorten debugging scripts
    Also bump to Clang 4

--- cppcheck-build.sh
+++ cppcheck-build.sh
@@ -2,7 +2,7 @@
 rm -rf build/
 mkdir build
 cd build
-cmake -G Ninja -DCGAL_DONT_OVERRIDE_CMAKE_FLAGS=True -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 ninja
 cppcheck --project=compile_commands.json --enable=all --force
 cd ..
--- scan-build.sh
+++ scan-build.sh
@@ -4,6 +4,6 @@
 rm -rf build/
 mkdir build
 cd build
-cmake -G Ninja -DCGAL_DONT_OVERRIDE_CMAKE_FLAGS=True -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/local/Cellar/llvm/3.9.1_1/share/clang/tools/scan-build/libexec/ccc-analyzer -DCMAKE_CXX_COMPILER=/usr/local/Cellar/llvm/3.9.1_1/share/clang/tools/scan-build/libexec/c++-analyzer ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/local/Cellar/llvm/4.0.0/share/clang/tools/scan-build/libexec/ccc-analyzer -DCMAKE_CXX_COMPILER=/usr/local/Cellar/llvm/4.0.0/share/clang/tools/scan-build/libexec/c++-analyzer ..
 #cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug ..
 /usr/local/opt/llvm/bin/scan-build -o $(pwd)/scanresults -v ninja
