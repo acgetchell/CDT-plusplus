a82fdd474ab3c5d1d2d554474a4d10d0a2e1bc40
Adam Getchell <acgetchell@ucdavis.edu>
2017-12-12 00:43:03 -0800

    Complete Vertex tests in Catch

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 rm -rf build/
 mkdir build && cd build
-cmake -G Ninja -DGMOCK_TESTS:BOOL=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -DGMOCK_TESTS:BOOL=OFF -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
--- tests/Vertex.cpp
+++ tests/Vertex.cpp
@@ -8,25 +8,17 @@
 /// @brief Tests on vertices
 /// @author Adam Getchell
 ///
-/// TODO: Fix cmake --build . --target test error from build directory
 
 #include "catch.hpp"
-#include "SimplicialManifold.h"
+#include "../src/SimplicialManifold.h"
 
-TEST_CASE("example/1=1", "Prove that one equals 1")
-{
-    int one = 1;
-    REQUIRE(one == 1);
-}
-
-/// TODO: Fix this test
 SCENARIO("Vertex operations", "[vertex]")
 {
     GIVEN("A Simplicial manifold")
     {
         SimplicialManifold universe;
 
-        WHEN("A vertex is inserted")
+        WHEN("A vertex is inserted.")
         {
             universe.triangulation->insert(Point(0,0,0));
 
@@ -34,7 +26,82 @@ SCENARIO("Vertex operations", "[vertex]")
             {
                 REQUIRE(universe.triangulation->number_of_vertices() == 1);
             }
+
+            THEN("Inserting 1 point should make dimension 0.")
+            {
+                REQUIRE(universe.triangulation->dimension() == 0);
+            }
+        }
+
+        WHEN("Two vertices are inserted.")
+        {
+            universe.triangulation->insert(Point(0,0,0));
+            universe.triangulation->insert(Point(1,0,0));
+
+            THEN("We should have 2 points inserted.")
+            {
+                REQUIRE(universe.triangulation->number_of_vertices() == 2);
+            }
+
+            THEN("Inserting 2 points should make dimension 1.")
+            {
+                REQUIRE(universe.triangulation->dimension() == 1);
+            }
+        }
+
+        WHEN("Three vertices are inserted.")
+        {
+            universe.triangulation->insert(Point(0,0,0));
+            universe.triangulation->insert(Point(1,0,0));
+            universe.triangulation->insert(Point(0,1,0));
+
+            THEN("We should have 3 points inserted.")
+            {
+                REQUIRE(universe.triangulation->number_of_vertices() == 3);
+            }
+
+            THEN("Inserting 3 points should make dimension 2.")
+            {
+                REQUIRE(universe.triangulation->dimension() == 2);
+            }
+        }
+
+        WHEN("Four vertices are inserted.")
+        {
+            universe.triangulation->insert(Point(0,0,0));
+            universe.triangulation->insert(Point(1,0,0));
+            universe.triangulation->insert(Point(0,1,0));
+            universe.triangulation->insert(Point(0,0,1));
+
+            THEN("We should have 4 points inserted.")
+            {
+                REQUIRE(universe.triangulation->number_of_vertices() == 4);
+            }
+
+            THEN("Dimensionality after 4 points should still be 3.")
+            {
+                REQUIRE(universe.triangulation->dimension() == 3);
+            }
+        }
+
+        WHEN("Six vertices are inserted.")
+        {
+            universe.triangulation->insert(Point(0,0,0));
+            universe.triangulation->insert(Point(1,0,0));
+            universe.triangulation->insert(Point(0,1,0));
+            universe.triangulation->insert(Point(0,0,1));
+            universe.triangulation->insert(Point(2,2,2));
+            universe.triangulation->insert(Point(-1,0,1));
+
+            THEN("We should have 6 points inserted.")
+            {
+                REQUIRE(universe.triangulation->number_of_vertices() == 6);
+            }
+
+            THEN("Dimensionality after 6 points should still be 3.")
+            {
+                REQUIRE(universe.triangulation->dimension() == 3);
+            }
         }
     }
 }
-
