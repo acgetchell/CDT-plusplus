be8bd08757aba522939958223278d6281b19ea85
Adam Getchell <acgetchell@ucdavis.edu>
2017-12-13 00:26:09 -0800

    Revert to previous Travis image

--- tests/FunctionRef.cpp
+++ tests/FunctionRef.cpp
@@ -11,42 +11,34 @@
 #include "catch.hpp"
 #include <Function_ref.h>
 #include <S3ErgodicMoves.h>
+#include <SimplicialManifold.h>
 #include <utility>
 
 SCENARIO("Lambda operations", "[function_ref]")
 {
-    GIVEN("A simple lambda")
+  GIVEN("A simple lambda")
+  {
+    auto increment_lambda = [](int a) { return ++a; };
+
+    WHEN("Lambda is called with 0.")
     {
-        auto increment_lambda = [](int a){return ++a;};
+      auto result = increment_lambda(0);
 
-        WHEN("Lambda is called with 0.")
-        {
-            auto result = increment_lambda(0);
-
-            THEN("We should get 1.")
-            {
-                REQUIRE(result == 1);
-            }
-        }
-
-        WHEN("Lambda is called with 1.")
-        {
-            auto result = increment_lambda(1);
-
-            THEN("We should get 2.")
-            {
-                REQUIRE(result == 2);
-            }
-        }
-
-        WHEN("Lambda is called with 5.")
-        {
-            auto result = increment_lambda(5);
-
-            THEN("We should get 6.")
-            {
-                REQUIRE(result == 6);
-            }
-        }
+      THEN("We should get 1.") { REQUIRE(result == 1); }
     }
+
+    WHEN("Lambda is called with 1.")
+    {
+      auto result = increment_lambda(1);
+
+      THEN("We should get 2.") { REQUIRE(result == 2); }
+    }
+
+    WHEN("Lambda is called with 5.")
+    {
+      auto result = increment_lambda(5);
+
+      THEN("We should get 6.") { REQUIRE(result == 6); }
+    }
+  }
 }
--- tests/Vertex.cpp
+++ tests/Vertex.cpp
@@ -14,94 +14,94 @@
 
 SCENARIO("Vertex operations", "[vertex]")
 {
-    GIVEN("A Simplicial manifold")
+  GIVEN("A Simplicial manifold")
+  {
+    SimplicialManifold universe;
+
+    WHEN("A vertex is inserted.")
     {
-        SimplicialManifold universe;
+      universe.triangulation->insert(Point(0, 0, 0));
 
-        WHEN("A vertex is inserted.")
-        {
-            universe.triangulation->insert(Point(0,0,0));
+      THEN("We should have 1 point inserted.")
+      {
+        REQUIRE(universe.triangulation->number_of_vertices() == 1);
+      }
 
-            THEN("We should have 1 point inserted.")
-            {
-                REQUIRE(universe.triangulation->number_of_vertices() == 1);
-            }
-
-            THEN("Inserting 1 point should make dimension 0.")
-            {
-                REQUIRE(universe.triangulation->dimension() == 0);
-            }
-        }
-
-        WHEN("Two vertices are inserted.")
-        {
-            universe.triangulation->insert(Point(0,0,0));
-            universe.triangulation->insert(Point(1,0,0));
-
-            THEN("We should have 2 points inserted.")
-            {
-                REQUIRE(universe.triangulation->number_of_vertices() == 2);
-            }
-
-            THEN("Inserting 2 points should make dimension 1.")
-            {
-                REQUIRE(universe.triangulation->dimension() == 1);
-            }
-        }
-
-        WHEN("Three vertices are inserted.")
-        {
-            universe.triangulation->insert(Point(0,0,0));
-            universe.triangulation->insert(Point(1,0,0));
-            universe.triangulation->insert(Point(0,1,0));
-
-            THEN("We should have 3 points inserted.")
-            {
-                REQUIRE(universe.triangulation->number_of_vertices() == 3);
-            }
-
-            THEN("Inserting 3 points should make dimension 2.")
-            {
-                REQUIRE(universe.triangulation->dimension() == 2);
-            }
-        }
-
-        WHEN("Four vertices are inserted.")
-        {
-            universe.triangulation->insert(Point(0,0,0));
-            universe.triangulation->insert(Point(1,0,0));
-            universe.triangulation->insert(Point(0,1,0));
-            universe.triangulation->insert(Point(0,0,1));
-
-            THEN("We should have 4 points inserted.")
-            {
-                REQUIRE(universe.triangulation->number_of_vertices() == 4);
-            }
-
-            THEN("Dimensionality after 4 points should still be 3.")
-            {
-                REQUIRE(universe.triangulation->dimension() == 3);
-            }
-        }
-
-        WHEN("Six vertices are inserted.")
-        {
-            universe.triangulation->insert(Point(0,0,0));
-            universe.triangulation->insert(Point(1,0,0));
-            universe.triangulation->insert(Point(0,1,0));
-            universe.triangulation->insert(Point(0,0,1));
-            universe.triangulation->insert(Point(2,2,2));
-            universe.triangulation->insert(Point(-1,0,1));
-
-            THEN("We should have 6 points inserted.")
-            {
-                REQUIRE(universe.triangulation->number_of_vertices() == 6);
-            }
-
-            THEN("Dimensionality after 6 points should still be 3.")
-            {
-                REQUIRE(universe.triangulation->dimension() == 3);
-            }
-        }
+      THEN("Inserting 1 point should make dimension 0.")
+      {
+        REQUIRE(universe.triangulation->dimension() == 0);
+      }
     }
+
+    WHEN("Two vertices are inserted.")
+    {
+      universe.triangulation->insert(Point(0, 0, 0));
+      universe.triangulation->insert(Point(1, 0, 0));
+
+      THEN("We should have 2 points inserted.")
+      {
+        REQUIRE(universe.triangulation->number_of_vertices() == 2);
+      }
+
+      THEN("Inserting 2 points should make dimension 1.")
+      {
+        REQUIRE(universe.triangulation->dimension() == 1);
+      }
+    }
+
+    WHEN("Three vertices are inserted.")
+    {
+      universe.triangulation->insert(Point(0, 0, 0));
+      universe.triangulation->insert(Point(1, 0, 0));
+      universe.triangulation->insert(Point(0, 1, 0));
+
+      THEN("We should have 3 points inserted.")
+      {
+        REQUIRE(universe.triangulation->number_of_vertices() == 3);
+      }
+
+      THEN("Inserting 3 points should make dimension 2.")
+      {
+        REQUIRE(universe.triangulation->dimension() == 2);
+      }
+    }
+
+    WHEN("Four vertices are inserted.")
+    {
+      universe.triangulation->insert(Point(0, 0, 0));
+      universe.triangulation->insert(Point(1, 0, 0));
+      universe.triangulation->insert(Point(0, 1, 0));
+      universe.triangulation->insert(Point(0, 0, 1));
+
+      THEN("We should have 4 points inserted.")
+      {
+        REQUIRE(universe.triangulation->number_of_vertices() == 4);
+      }
+
+      THEN("Dimensionality after 4 points should still be 3.")
+      {
+        REQUIRE(universe.triangulation->dimension() == 3);
+      }
+    }
+
+    WHEN("Six vertices are inserted.")
+    {
+      universe.triangulation->insert(Point(0, 0, 0));
+      universe.triangulation->insert(Point(1, 0, 0));
+      universe.triangulation->insert(Point(0, 1, 0));
+      universe.triangulation->insert(Point(0, 0, 1));
+      universe.triangulation->insert(Point(2, 2, 2));
+      universe.triangulation->insert(Point(-1, 0, 1));
+
+      THEN("We should have 6 points inserted.")
+      {
+        REQUIRE(universe.triangulation->number_of_vertices() == 6);
+      }
+
+      THEN("Dimensionality after 6 points should still be 3.")
+      {
+        REQUIRE(universe.triangulation->dimension() == 3);
+      }
+    }
+  }
 }
