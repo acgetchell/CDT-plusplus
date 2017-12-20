2cdc57b091c00f0f9112c7a9533bbb2702cdc87d
Adam Getchell <acgetchell@ucdavis.edu>
2017-12-19 20:26:24 -0800

    Add MeasurementTests

---
+++ tests/MeasurementsTest.cpp
@@ -0,0 +1,54 @@
+/// Causal Dynamical Triangulations in C++ using CGAL
+///
+/// Copyright © 2016-2017 Adam Getchell
+///
+/// Checks various measurement functions perform correctly.
+
+/// @file MeasurementsTest.cpp
+/// @brief Tests for the various measurement functions
+/// @author Adam Getchell
+///
+/// @todo Fix PersistData test
+
+#include "catch.hpp"
+#include <Measurements.h>
+#include <S3ErgodicMoves.h>
+
+SCENARIO("Take measurements on a Simplicial Manifold.", "[measurements][!mayfail]")
+{
+  GIVEN("A correctly-constructed SimplicialManifold.")
+  {
+    constexpr auto simplices = static_cast<std::intmax_t>(6400);
+    constexpr auto timeslices = static_cast<std::intmax_t>(7);
+    SimplicialManifold      universe(make_triangulation(simplices, timeslices));
+    // It is correctly constructed
+    CHECK(universe.triangulation);
+    CHECK(universe.geometry->number_of_cells() ==
+            universe.triangulation->number_of_finite_cells());
+    CHECK(universe.geometry->number_of_edges() ==
+            universe.triangulation->number_of_finite_edges());
+    CHECK(universe.geometry->N0() ==
+            universe.triangulation->number_of_vertices());
+    CHECK(universe.triangulation->dimension() == 3);
+    CHECK(fix_timeslices(universe.triangulation));
+    CHECK(universe.triangulation->is_valid());
+    CHECK(universe.triangulation->tds().is_valid());
+    WHEN("We measure volume per time slice.")
+    {
+      VolumePerTimeslice(universe);
+      THEN("Valid results are returned.")
+      {
+        REQUIRE_FALSE(universe.geometry->getSpacelike_facets()->empty());
+        REQUIRE(universe.geometry->max_timevalue().get() == timeslices);
+      }
+      THEN("Results are persisted across moves.")
+      {
+        Move_tracker attempted_moves;
+        auto result = make_26_move(std::move(universe), attempted_moves);
+        REQUIRE(attempted_moves[2] > 0);
+        CHECK(result.geometry->getSpacelike_facets().is_initialized());
+        CHECK(result.geometry->max_timevalue().is_initialized());
+      }
+    }
+  }
+}
