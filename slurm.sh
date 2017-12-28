c3245909c94967293bade4035b7006f1f41c082a
Adam Getchell <acgetchell@ucdavis.edu>
2017-12-27 18:04:07 -0800

    Project cleanup

--- slurm.sh
+++ slurm.sh
@@ -4,7 +4,7 @@
 module load cmake gcc boost cgal tbb
 rm -rf build/
 mkdir build && cd build
-cmake -G Ninja -DGMOCK_TESTS:BOOL=OFF -DCMAKE_BUILD_TYPE=Debug ..
+cmake -G Ninja -DTESTS:BOOL=OFF -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 mkdir $HOME/data/`date +%Y-%m-%d.%R%Z`
 cp cdt-opt $HOME/data/`date +%Y-%m-%d.%R%Z`/
