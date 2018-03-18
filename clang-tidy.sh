05a21116a393108d80b3242908c911b529e32dda
Adam Getchell <acgetchell@ucdavis.edu>
2018-03-18 11:28:42 -0700

    Update ClangTidy

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -4,4 +4,5 @@ rm -rf build/
 mkdir build && cd build
 # Export compilation database for run-clang-tidy.py
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-`locate run-clang-tidy.py` -header-filter='.*' -checks='-*,cert*,clang*,cppcoreguidelines*,misc-assert*,misc-b*,misc-dangling*,misc-f*,misc-i*,misc-move-const*,misc-n*,misc-r*,misc-s*,misc-t*,misc-u*,misc-v*,modernize*,performance*' -fix
+#`locate run-clang-tidy.py` -header-filter='.*' -checks='-*,cert*,clang*,cppcoreguidelines*,misc-assert*,misc-b*,misc-dangling*,misc-f*,misc-i*,misc-move-const*,misc-n*,misc-r*,misc-s*,misc-t*,misc-u*,misc-v*,modernize*,performance*' -fix
+`locate run-clang-tidy.py` -fix
