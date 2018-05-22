b1749b3bc9fca756d2d4e70e76d9105c2142aef0
Adam Getchell <acgetchell@ucdavis.edu>
2018-05-22 09:00:09 -0700

    Add comet.ml parameter tuning
    Initial version

---
+++ src/optimize-initialize.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, division, print_function
+
+# Import comet
+from comet_ml import Experiment
+
+# Run command line programs
+from subprocess import Popen
+import shlex
+
+# Create an experiment with api key
+experiment = Experiment(api_key="***REMOVED***", project_name="cdt-plusplus", team_name="ucdavis")
+
+import os
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+
+tf.enable_eager_execution()
+
+print('TensorFlow version: {}'.format(tf.VERSION))
+
+hyper_params = {'simplices': 32000, 'foliations': 12 }
+experiment.log_multiple_params(hyper_params)
+
+command_line = "../build/initialize --s -n" + str(experiment.get_parameter("simplices")) + " -t" + str(experiment.get_parameter("foliations"))
+args = shlex.split(command_line)
+
+print(args)
+
+process = Popen(args)
+
+#process = Popen(["../build/initialize", "--s", "-n16000", "-t11"])
+(output, err) = process.communicate()
+exit_code = process.wait()
+
+print(output)
