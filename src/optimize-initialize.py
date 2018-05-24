7f6896f0510c68f66cd61bd50224d32f0592aac8
Adam Getchell <acgetchell@ucdavis.edu>
2018-05-24 13:18:54 -0700

    Add Optimizer

--- src/optimize-initialize.py
+++ src/optimize-initialize.py
@@ -2,10 +2,21 @@ from __future__ import absolute_import, division, print_function
 
 # Import comet
 from comet_ml import Experiment
+from comet_ml import Optimizer
 
 # Run command line programs
 from subprocess import Popen
 import shlex
+from subprocess import check_output as qx
+
+# Create an optimizer for dynamic parameters
+optimizer = Optimizer(api_key="***REMOVED***")
+params = """
+init_radius real [1, 10] [1.0]
+foliation_spacing real [1, 10] [1.0]
+"""
+
+optimizer.set_params(params)
 
 # Create an experiment with api key
 experiment = Experiment(api_key="***REMOVED***", project_name="cdt-plusplus", team_name="ucdavis")
@@ -18,17 +29,18 @@ tf.enable_eager_execution()
 
 print('TensorFlow version: {}'.format(tf.VERSION))
 
-hyper_params = {'simplices': 32000, 'foliations': 12 }
+hyper_params = {'simplices': 12000, 'foliations': 12}
 experiment.log_multiple_params(hyper_params)
 
-command_line = "../build/initialize --s -n" + str(experiment.get_parameter("simplices")) + " -t" + str(experiment.get_parameter("foliations"))
+command_line = "../build/initialize --s -n" + str(experiment.get_parameter("simplices")) \
+               + " -t" + str(experiment.get_parameter("foliations"))
 args = shlex.split(command_line)
 
 print(args)
 
 process = Popen(args)
 
-#process = Popen(["../build/initialize", "--s", "-n16000", "-t11"])
+# process = Popen(["../build/initialize", "--s", "-n16000", "-t11"])
 (output, err) = process.communicate()
 exit_code = process.wait()
 
