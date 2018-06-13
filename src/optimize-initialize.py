7dcb276a76483fba2253fe761dc5c101d726e6bf
Adam Getchell <acgetchell@ucdavis.edu>
2018-06-12 19:55:48 -0700

    Basic parameter varying and reporting
    Uses Comet.ml Optimizer, Suggestion, and undocumented exceptions to properly handle a variety of suggested parameters.

--- src/optimize-initialize.py
+++ src/optimize-initialize.py
@@ -1,3 +1,5 @@
+# Usage: python optimize-initialize.py
+#
 from __future__ import absolute_import, division, print_function
 
 import os
@@ -5,6 +7,7 @@ import os
 # Import Comet.ml
 from comet_ml import Experiment
 from comet_ml import Optimizer
+import comet_ml as cm
 
 # Import TensorFlow
 import tensorflow as tf
@@ -18,38 +21,54 @@ import re
 # Create an optimizer for dynamic parameters
 optimizer = Optimizer(api_key="***REMOVED***")
 params = """
-init_radius real [1, 10] [1.0]
-foliation_spacing real [1, 10] [1.0]
+init_radius integer [1, 3] [1]
+foliation_spacing integer [1, 3] [1]
 """
 
 optimizer.set_params(params)
 
-# Create an experiment with api key
-experiment = Experiment(api_key="***REMOVED***", project_name="cdt-plusplus", team_name="ucdavis")
-
-
 tf.enable_eager_execution()
 
-print('TensorFlow version: {}'.format(tf.VERSION))
+try:
+    while True:
+        # Get a suggestion
+        suggestion = optimizer.get_suggestion()
 
-hyper_params = {'simplices': 12000, 'foliations': 12}
-experiment.log_multiple_params(hyper_params)
+        # Create an experiment with api key
+        experiment = Experiment(api_key="***REMOVED***", project_name="cdt-plusplus", team_name="ucdavis")
 
-command_line = "../build/initialize --s -n" + str(experiment.get_parameter("simplices")) \
+        print('TensorFlow version: {}'.format(tf.VERSION))
+
+        hyper_params = {'simplices': 12000, 'foliations': 12}
+        experiment.log_multiple_params(hyper_params)
+
+        command_line = "../build/initialize --s -n" + str(experiment.get_parameter("simplices")) \
                + " -t" + str(experiment.get_parameter("foliations"))
-args = shlex.split(command_line)
+        args = shlex.split(command_line)
 
-# print(args)
+        # print(args)
 
-output = qx(args)
+        output = qx(args)
 
-# Parse output for final simplices, which is the label
-result = 0
-for line in output.splitlines():
-    if line.startswith("Final number"):
-        # print(line)
-        s = re.findall('\d+', line)
-        # print(s)
-        result = float(s[0])
+        # Parse output for final simplices, which is the label
+        result = 0
+        for line in output.splitlines():
+            if line.startswith("Final number"):
+                # print(line)
+                s = re.findall('\d+', line)
+                # print(s)
+                result = float(s[0])
 
-print(result)
+        print(result)
+        print('Initial radius is: {}'.format(suggestion["init_radius"]))
+        print('Foliation spacing is: {}'.format(suggestion["foliation_spacing"]))
+
+        # Score model
+        score = ((result - experiment.get_parameter("simplices"))/(experiment.get_parameter("simplices")))*100
+        suggestion.report_score("%Error", score)
+
+except cm.exceptions.NoMoreSuggestionsAvailable as NoMore:
+    print("No more suggestions")
+
+finally:
+    print("All done with parameter optimization, look at Comet.ml for results.")
