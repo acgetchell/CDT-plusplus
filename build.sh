93bd9bab60aa3787447900b027107961390c6c38
Adam Getchell <acgetchell@ucdavis.edu>
2018-08-19 12:00:59 -0700

    Show conan_cmake_run(BUILD missing) isn't working  [skip ci]

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 rm -rf build/
 mkdir build && cd build
-cmake -G Ninja -DTESTS:BOOL=ON -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
+#cmake -G Ninja -DTESTS:BOOL=ON -DCMAKE_BUILD_TYPE=Debug ..
+#cmake --build .
+
+conan install .. -pr cdt
+conan build ..
+#ctest
