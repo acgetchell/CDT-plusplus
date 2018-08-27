8b4a3f849267491070d06f88b15574c2b46c7b30
Adam Getchell <acgetchell@ucdavis.edu>
2018-08-27 12:31:17 -0700

    Use Conan for H. Hinnant's date library

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,8 @@ from conans import ConanFile, CMake
 class CausalDynamicalTriangulations(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "boost/1.68.0@conan/stable", "catch2/2.3.0@bincrafters/stable", "TBB/2018_U5@conan/stable",\
-               "eigen/3.3.5@conan/stable", "libcurl/7.60.0@bincrafters/stable", "docopt/0.6.2@conan/stable"
+               "eigen/3.3.5@conan/stable", "libcurl/7.60.0@bincrafters/stable", "docopt/0.6.2@conan/stable",\
+               "date/2.4@bincrafters/stable"
     generators = "cmake"
     default_options = "boost:without_thread=False"
     # default_options = "Boost:header_only=True"
