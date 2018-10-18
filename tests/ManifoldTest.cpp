9422940625012185b64ad7059d24e4c59df26296
Adam Getchell <acgetchell@ucdavis.edu>
2018-10-17 22:40:30 -0700

    Added all tests from S3TriangulationTest into ManifoldTest

--- tests/ManifoldTest.cpp
+++ tests/ManifoldTest.cpp
@@ -11,6 +11,8 @@
 #include <Manifold.hpp>
 #include <catch2/catch.hpp>
 
+using namespace std;
+
 SCENARIO("3-Manifold exception-safety", "[manifold]")
 {
   GIVEN("A 3-dimensional manifold.")
@@ -19,29 +21,39 @@ SCENARIO("3-Manifold exception-safety", "[manifold]")
     {
       THEN("It is no-throw default constructible.")
       {
-        CHECK_FALSE(std::is_nothrow_default_constructible<Manifold3>::value);
+        CHECK_FALSE(is_nothrow_default_constructible<Manifold3>::value);
       }
       THEN("It is no-throw destructible.")
       {
-        REQUIRE(std::is_nothrow_destructible<Manifold3>::value);
+        REQUIRE(is_nothrow_destructible<Manifold3>::value);
       }
       THEN("It is no-throw copy constructible.")
       {
-        CHECK_FALSE(std::is_nothrow_copy_constructible<Manifold3>::value);
+        CHECK_FALSE(is_nothrow_copy_constructible<Manifold3>::value);
       }
       THEN("It is no-throw copy assignable.")
       {
-        CHECK_FALSE(std::is_nothrow_copy_assignable<Manifold3>::value);
+        CHECK_FALSE(is_nothrow_copy_assignable<Manifold3>::value);
       }
       THEN("It is no-throw move constructible.")
       {
-        REQUIRE(std::is_nothrow_move_constructible<Manifold3>::value);
+        REQUIRE(is_nothrow_move_constructible<Manifold3>::value);
       }
       THEN("It is no-throw move assignable.")
       {
-        REQUIRE(std::is_nothrow_move_assignable<Manifold3>::value);
+        REQUIRE(is_nothrow_move_assignable<Manifold3>::value);
       }
     }
+  }
+}
+
+SCENARIO("Delaunay std::unique_ptr", "[manifold]")
+{
+  WHEN("A unique pointer to a Delaunay triangulation is created.")
+  {
+    Delaunay3 universe;
+    auto      universe_ptr = std::make_unique<decltype(universe)>(universe);
+    THEN("It is not null.") { REQUIRE(universe_ptr); }
   }
 }
 
@@ -60,20 +72,22 @@ SCENARIO("3-Manifold initialization", "[manifold]")
     WHEN("It is constructed from a Delaunay triangulation.")
     {
       Causal_vertices cv;
-      cv.emplace_back(std::make_pair(Point(0, 0, 0), 1));
-      cv.emplace_back(std::make_pair(Point(1, 0, 1), 2));
-      cv.emplace_back(std::make_pair(Point(0, 1, 1), 2));
-      cv.emplace_back(std::make_pair(Point(1, 1, 1), 2));
-      cv.emplace_back(std::make_pair(Point(1, 1, 2), 3));
+      cv.emplace_back(make_pair(Point(0, 0, 0), 1));
+      cv.emplace_back(make_pair(Point(1, 0, 1), 2));
+      cv.emplace_back(make_pair(Point(0, 1, 1), 2));
+      cv.emplace_back(make_pair(Point(1, 1, 1), 2));
+      cv.emplace_back(make_pair(Point(1, 1, 2), 3));
       Delaunay3 dt(cv.begin(), cv.end());
       Manifold3 manifold(dt);
 
       THEN("The triangulation is valid.")
       {
+        REQUIRE(manifold.getTriangulation()->is_valid());
         REQUIRE(manifold.getTriangulation()->tds().is_valid());
       }
       THEN("The geometry matches the triangulation.")
       {
+        REQUIRE(fix_timeslices(manifold.getTriangulation()));
         REQUIRE(manifold.getGeometry().N0() == 5);
         REQUIRE(manifold.getGeometry().N1_SL() == 3);
         REQUIRE(manifold.getGeometry().N1_TL() == 6);
@@ -82,13 +96,51 @@ SCENARIO("3-Manifold initialization", "[manifold]")
         REQUIRE(manifold.getGeometry().min_time() == 1);
         REQUIRE(manifold.getGeometry().max_time() == 3);
         // Human verification
+        print_manifold(manifold);
         manifold.getGeometry().print_volume_per_timeslice();
       }
     }
-    WHEN("It is constructed with desired_simplices and desired_timeslices.")
+    WHEN("Constructing the minimum size triangulation.")
     {
-      std::int_fast64_t desired_simplices{640};
-      std::int_fast64_t desired_timeslices{4};
+      int_fast64_t desired_simplices{2};
+      int_fast64_t desired_timeslices{2};
+      Manifold3    manifold(desired_simplices, desired_timeslices);
+      THEN("Triangulation is valid.")
+      {
+        REQUIRE(manifold.getTriangulation()->is_valid());
+        REQUIRE(manifold.getTriangulation()->tds().is_valid());
+      }
+      THEN("The geometry matches the triangulation.")
+      {
+        REQUIRE(fix_timeslices(manifold.getTriangulation()));
+        REQUIRE(manifold.getTriangulation()->number_of_vertices() ==
+                manifold.getGeometry().N0());
+        REQUIRE(manifold.getTriangulation()->number_of_finite_edges() ==
+                manifold.getGeometry().N1());
+        REQUIRE(manifold.getTriangulation()->number_of_finite_facets() ==
+                manifold.getGeometry().N2());
+        REQUIRE(manifold.getTriangulation()->number_of_finite_cells() ==
+                manifold.getGeometry().N3());
+        // We have 1 to 8 vertices
+        auto vertices{manifold.getGeometry().N0()};
+        CHECK(1 << vertices);
+        CHECK(vertices <= 8);
+        // We have 1 to 12 cells
+        auto cells{manifold.getGeometry().N3()};
+        CHECK(1 <= cells);
+        CHECK(cells <= 12);
+        // We have all the time values
+        CHECK(manifold.getGeometry().min_time() == 1);
+        CHECK(manifold.getGeometry().max_time() == desired_timeslices);
+        // Human verification
+        print_manifold(manifold);
+        manifold.getGeometry().print_volume_per_timeslice();
+      }
+    }
+    WHEN("Constructing a small triangulation.")
+    {
+      int_fast64_t desired_simplices{640};
+      int_fast64_t desired_timeslices{4};
       Manifold3   manifold(desired_simplices, desired_timeslices);
       THEN("Triangulation is valid.")
       {
@@ -97,6 +149,7 @@ SCENARIO("3-Manifold initialization", "[manifold]")
       }
       THEN("The geometry matches the triangulation.")
       {
+        REQUIRE(fix_timeslices(manifold.getTriangulation()));
         REQUIRE(manifold.getTriangulation()->number_of_vertices() ==
                 manifold.getGeometry().N0());
         REQUIRE(manifold.getTriangulation()->number_of_finite_edges() ==
@@ -106,6 +159,33 @@ SCENARIO("3-Manifold initialization", "[manifold]")
         REQUIRE(manifold.getTriangulation()->number_of_finite_cells() ==
                 manifold.getGeometry().N3());
         // Human verification
+        print_manifold(manifold);
+        manifold.getGeometry().print_volume_per_timeslice();
+      }
+    }
+    WHEN("Constructing a medium triangulation.")
+    {
+      int_fast64_t desired_simplices{6400};
+      int_fast64_t desired_timeslices{7};
+      Manifold3    manifold(desired_simplices, desired_timeslices);
+      THEN("Triangulation is valid.")
+      {
+        REQUIRE(manifold.getTriangulation()->is_valid());
+        REQUIRE(manifold.getTriangulation()->tds().is_valid());
+      }
+      THEN("The geometry matches the triangulation.")
+      {
+        REQUIRE(fix_timeslices(manifold.getTriangulation()));
+        REQUIRE(manifold.getTriangulation()->number_of_vertices() ==
+                manifold.getGeometry().N0());
+        REQUIRE(manifold.getTriangulation()->number_of_finite_edges() ==
+                manifold.getGeometry().N1());
+        REQUIRE(manifold.getTriangulation()->number_of_finite_facets() ==
+                manifold.getGeometry().N2());
+        REQUIRE(manifold.getTriangulation()->number_of_finite_cells() ==
+                manifold.getGeometry().N3());
+        // Human verification
+        print_manifold(manifold);
         manifold.getGeometry().print_volume_per_timeslice();
       }
     }
