54b4346b96f58a9870b8b94cb5f5fd0b8bc9fe4b
Adam Getchell <acgetchell@ucdavis.edu>
2018-11-08 17:15:30 -0800

    Update Conan packages
    Except TBB, which breaks with clang

--- conanfile.py
+++ conanfile.py
@@ -2,9 +2,9 @@ from conans import ConanFile, CMake
 
 class CausalDynamicalTriangulations(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "boost/1.68.0@conan/stable", "catch2/2.4.0@bincrafters/stable", "TBB/2018_U5@conan/stable",\
+    requires = "boost/1.68.0@conan/stable", "catch2/2.4.1@bincrafters/stable", "TBB/2018_U5@conan/stable",\
                "eigen/3.3.5@conan/stable", "docopt/0.6.2@conan/stable",\
-               "date/2.4.1@bincrafters/stable", "gsl_microsoft/1.0.0@bincrafters/stable"
+               "date/2.4.1@bincrafters/stable", "gsl_microsoft/2.0.0@bincrafters/stable"
     generators = "cmake"
     default_options = "boost:without_thread=False"
     # default_options = "Boost:header_only=True"
@@ -17,4 +17,4 @@ class CausalDynamicalTriangulations(ConanFile):
         cmake.build()
 
     def build_requirements(self):
-        self.build_requires("cmake_installer/3.11.3@conan/stable")
+        self.build_requires("cmake_installer/3.12.0@conan/stable")
