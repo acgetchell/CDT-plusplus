d61f80496287a85880010e573d9e2c4975046870
Adam Getchell <acgetchell@ucdavis.edu>
2018-12-18 23:13:47 -0800

    Okay, Boost required for Conan
    and HomeBrew/LinuxBrew install it as it's a dependency for CGAL, so doubly Boosted.

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class CausalDynamicalTriangulations(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "catch2/2.4.1@bincrafters/stable", \
+    requires = "boost/1.68.0@conan/stable", "catch2/2.4.1@bincrafters/stable", \
                "docopt/0.6.2@conan/stable",\
                "date/2.4.1@bincrafters/stable", "gsl_microsoft/2.0.0@bincrafters/stable"
     generators = "cmake"
