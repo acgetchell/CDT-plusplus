f2dd6d737fd7275910b2e0b3af369d4be04c50e2
Adam Getchell <acgetchell@ucdavis.edu>
2019-04-02 16:52:50 -0700

    Establish that triangulation is getting changed by the move
    But update_geometry() throws. Also fix ClangTidy and ClangFormat. Try to fix conan by telling it to use clang-7. It's still broken due to not finding MPFR, which is already installed in the Travis image.

--- tests/Move_command_test.cpp
+++ tests/Move_command_test.cpp
@@ -121,12 +121,12 @@ SCENARIO("Applying the Move Command", "[move3]")
     WHEN("A (2,3) move is queued")
     {
       MoveCommand command(manifold);
-      auto    move23 = [](Manifold3& manifold) -> decltype(auto) {
+      auto        move23 = [](Manifold3& manifold) -> decltype(auto) {
         return manifold3_moves::do_23_move(manifold);
       };
-//      auto func(manifold3_moves::do_23_move);
+      //      auto func(manifold3_moves::do_23_move);
       command.enqueue(move23);
-//      command.enqueue(func);
+      //      command.enqueue(func);
       THEN("It is executed correctly")
       {
         CAPTURE(command.get_manifold().N3_22());
@@ -137,16 +137,25 @@ SCENARIO("Applying the Move Command", "[move3]")
         auto* manifold_ptr = &manifold;
         auto* result_ptr   = &result;
         REQUIRE_FALSE(manifold_ptr == result_ptr);
-        // Exception thrown on following call
-        result.update_geometry();
+        // Did the triangulation actually change? We should have +1 cell
+        CHECK(result.get_triangulation()
+                  .get_delaunay()
+                  .number_of_finite_cells() ==
+              manifold.get_triangulation()
+                      .get_delaunay()
+                      .number_of_finite_cells() +
+                  1);
+        // Now we should update the geometry data structure, but
+        // an exception is thrown on following call
+        //        result.update_geometry();
         // These should be +1 after command
         CAPTURE(result.N3_22());
         CAPTURE(result.N1_TL());
         cout << "After move.\n";
         print_manifold_details(result);
         // Not calling update makes this test fail
-//        CHECK(manifold3_moves::check_move(
-//            manifold, result, manifold3_moves::move_type::TWO_THREE));
+        //        CHECK(manifold3_moves::check_move(
+        //            manifold, result, manifold3_moves::move_type::TWO_THREE));
       }
     }
   }
