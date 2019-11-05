998ef800dd8c623c515d8f7e297f3dde6e6bc897
Adam Getchell <acgetchell@ucdavis.edu>
2019-11-05 08:41:15 -0800

    Fix Coveralls

--- tools/kcov.sh
+++ tools/kcov.sh
@@ -4,7 +4,7 @@ cd ..
 cd build || exit
 cd tests || exit
 mkdir kcov-results
-kcov --exclude-pattern=usr/include,lib/system,vcpkg --include-pattern=CDT-plusplus/src,CDT-plusplus/include kcov-results CDT_test &
+kcov --coveralls-id="$TRAVIS_JOB_ID" --exclude-pattern=usr/include,lib/system,vcpkg --include-pattern=CDT-plusplus/src,CDT-plusplus/include kcov-results CDT_test &
 # kill process after 10 minutes
 sleep 600
 pkill -9 kcov
