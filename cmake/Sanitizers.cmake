6d2b79d95a7e22fc18b74d53630f28bf279fa2ac
Adam Getchell <acgetchell@ucdavis.edu>
2020-03-03 21:11:33 -0800

    Configure Sanitizers and Compiler Warnings

---
+++ cmake/Sanitizers.cmake
@@ -0,0 +1,70 @@
+# Derived from:
+#
+# https://github.com/lefticus/cpp_starter_project/blob/master/cmake/Sanitizers.cmake
+#
+
+function(enable_sanitizers project_name)
+
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL
+                                             "Clang")
+    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
+
+    if(ENABLE_COVERAGE)
+      target_compile_options(project_options INTERFACE --coverage -O0 -g)
+      target_link_libraries(project_options INTERFACE --coverage)
+      message(STATUS "Coverage enabled.")
+    endif()
+
+    option(ENABLE_VALGRIND "Enable Valgrind" OFF)
+    if(ENABLE_VALGRIND)
+      target_compile_options(project_options INTERFACE -g -O0
+                                                       -fsanitize=address)
+      target_link_libraries(project_options INTERFACE -fsanitize=address)
+      message(STATUS "Valgrind enabled.")
+      set(MEMORYCHECK_COMMAND_OPTIONS
+          "${MEMORYCHECK_COMMAND_OPTIONS} --leak-check=full")
+      set(MEMORYCHECK_COMMAND_OPTIONS
+          "${MEMORYCHECK_COMMAND_OPTIONS} --track-fds=yes")
+      set(MEMORYCHECK_COMMAND_OPTIONS
+          "${MEMORYCHECK_COMMAND_OPTIONS} --trace-children=yes")
+      set(MEMORYCHECK_COMMAND_OPTIONS
+          "${MEMORYCHECK_COMMAND_OPTIONS} --error-exitcode=1")
+    endif()
+
+    set(SANITIZERS "")
+
+    option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" FALSE)
+    if(ENABLE_SANITIZER_ADDRESS)
+      list(APPEND SANITIZERS "address")
+    endif()
+
+    option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" FALSE)
+    if(ENABLE_SANITIZER_MEMORY)
+      list(APPEND SANITIZERS "memory")
+    endif()
+
+    option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR
+           "Enable undefined behavior sanitizer" FALSE)
+    if(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR)
+      list(APPEND SANITIZERS "undefined")
+    endif()
+
+    option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer" FALSE)
+    if(ENABLE_SANITIZER_THREAD)
+      list(APPEND SANITIZERS "thread")
+    endif()
+
+    list(JOIN SANITIZERS "," LIST_OF_SANITIZERS)
+
+  endif()
+
+  if(LIST_OF_SANITIZERS)
+    if(NOT "${LIST_OF_SANITIZERS}" STREQUAL "")
+      target_compile_options(${project_name}
+                             INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+      target_link_libraries(${project_name}
+                            INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+    endif()
+  endif()
+
+endfunction()
--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd ..
 rm -rf build/
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DENABLE_CCACHE=OFF -DCMAKE_TOOLCHAIN_FILE="$HOME"/vcpkg/scripts/buildsystems/vcpkg.cmake --trace-source=CMakeLists.txt
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DENABLE_CCACHE=OFF -DCMAKE_TOOLCHAIN_FILE="$HOME"/vcpkg/scripts/buildsystems/vcpkg.cmake --trace-source=CMakeLists.txt --trace-source=cmake/CompilerWarnings.cmake
 cmake --build build
