4a0dafb83222d5fdf38041684a876b583759729d
Adam Getchell (via Travis CI) <acgetchell@ucdavis.edu>
2020-10-30 02:57:28 +0000

    Deploy acgetchell/CDT-plusplus to github.com/acgetchell/CDT-plusplus.git:gh-pages

--- search/all_12.js
+++ search/all_12.js
@@ -3,7 +3,7 @@ var searchData=
   ['update',['update',['../class_manifold_3_013_01_4.html#a9e12eb966f3d2c5d96f088500282d04e',1,'Manifold&lt; 3 &gt;']]],
   ['update_5fgeometry',['update_geometry',['../class_manifold_3_013_01_4.html#a5ee28836c3c5c7f61c7a3da3172800ce',1,'Manifold&lt; 3 &gt;']]],
   ['update_5ftriangulation',['update_triangulation',['../class_manifold_3_013_01_4.html#ac8408f26d46686c4a2ffd191a0679622',1,'Manifold&lt; 3 &gt;']]],
-  ['usage',['USAGE',['../cdt-gv_8cpp.html#a44e58f0208f2c71a06729f1be06b55bc',1,'USAGE():&#160;cdt-gv.cpp'],['../cdt_8cpp.html#a6d6e26e7e3af2d4bd47f6582aff224f8',1,'USAGE():&#160;cdt.cpp'],['../initialize_8cpp.html#a44e58f0208f2c71a06729f1be06b55bc',1,'USAGE():&#160;initialize.cpp']]],
+  ['usage',['USAGE',['../cdt-gv_8cpp.html#aa6a50bed89859f476336638bbad895ac',1,'USAGE():&#160;cdt-gv.cpp'],['../cdt_8cpp.html#a6d6e26e7e3af2d4bd47f6582aff224f8',1,'USAGE():&#160;cdt.cpp'],['../initialize_8cpp.html#aa6a50bed89859f476336638bbad895ac',1,'USAGE():&#160;initialize.cpp']]],
   ['utilities_2ehpp',['Utilities.hpp',['../_utilities_8hpp.html',1,'']]],
   ['utilities_5ftest_2ecpp',['Utilities_test.cpp',['../_utilities__test_8cpp.html',1,'']]]
 ];
--- search/variables_d.js
+++ search/variables_d.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['usage',['USAGE',['../cdt-gv_8cpp.html#a44e58f0208f2c71a06729f1be06b55bc',1,'USAGE():&#160;cdt-gv.cpp'],['../cdt_8cpp.html#a6d6e26e7e3af2d4bd47f6582aff224f8',1,'USAGE():&#160;cdt.cpp'],['../initialize_8cpp.html#a44e58f0208f2c71a06729f1be06b55bc',1,'USAGE():&#160;initialize.cpp']]]
+  ['usage',['USAGE',['../cdt-gv_8cpp.html#aa6a50bed89859f476336638bbad895ac',1,'USAGE():&#160;cdt-gv.cpp'],['../cdt_8cpp.html#a6d6e26e7e3af2d4bd47f6582aff224f8',1,'USAGE():&#160;cdt.cpp'],['../initialize_8cpp.html#aa6a50bed89859f476336638bbad895ac',1,'USAGE():&#160;initialize.cpp']]]
 ];
