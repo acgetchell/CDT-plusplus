5ede2251da7497e9ea1bfd6edc152c0377f9df38
Adam Getchell (via Travis CI) <acgetchell@ucdavis.edu>
2020-12-05 08:36:45 +0000

    Deploy acgetchell/CDT-plusplus to github.com/acgetchell/CDT-plusplus.git:gh-pages

--- search/related_0.js
+++ search/related_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['swap',['swap',['../struct_geometry_3_013_01_4.html#a89aa633be477dfd525950f3b0e2d1bf8',1,'Geometry&lt; 3 &gt;::swap()'],['../class_manifold_3_013_01_4.html#aed56b4a1e59606b624184c0bd676f84d',1,'Manifold&lt; 3 &gt;::swap()'],['../struct_simplicial_manifold.html#a6b2f76aacf53811ed6cc2f36837c2ebf',1,'SimplicialManifold::swap()']]]
+  ['swap',['swap',['../class_foliated_triangulation_3_013_01_4.html#af87d0a9c0d31da0b4befc5d62a958641',1,'FoliatedTriangulation&lt; 3 &gt;::swap()'],['../struct_geometry_3_013_01_4.html#a89aa633be477dfd525950f3b0e2d1bf8',1,'Geometry&lt; 3 &gt;::swap()'],['../class_manifold_3_013_01_4.html#aed56b4a1e59606b624184c0bd676f84d',1,'Manifold&lt; 3 &gt;::swap()'],['../struct_simplicial_manifold.html#a6b2f76aacf53811ed6cc2f36837c2ebf',1,'SimplicialManifold::swap()']]]
 ];
