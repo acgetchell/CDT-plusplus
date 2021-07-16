dd011ac877e2632c43d994ae12a464e77c405c36
Adam Getchell <acgetchell@ucdavis.edu>
2021-07-16 12:16:36 -0700

    (ci) SonarCloud code coverage [ci skip]
    Using gcov, this will capture coverage on a lot of library files.

---
+++ scripts/gcov.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Before running this script, make sure $VCPKG_ROOT is set, e.g.
+# VCPKG_ROOT="$HOME"/vcpkg && export VCPKG_ROOT
+
+cd ..
+rm -rf build/
+cmake -G Ninja -D ENABLE_COVERAGE:BOOL=TRUE -D CMAKE_BUILD_TYPE=Debug -D ENABLE_TESTING:BOOL=TRUE -S . -B build
+cmake --build build
+cd build || exit
+ctest --output-on-failure -j8
+mkdir gcov-reports
+pushd gcov-reports
+for f in `find ../tests/CMakeFiles/CDT_test.dir -name '*.o'`; do
+  echo "Processing $f file..."
+  gcov -o ${f} x
+done
+ls | wc -l
+popd
