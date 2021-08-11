707aaa1739223af792b8d273c0d45fcdb372af7a
Adam Getchell (via Travis CI) <acgetchell@ucdavis.edu>
2021-08-11 19:56:38 +0000

    Deploy acgetchell/CDT-plusplus to github.com/acgetchell/CDT-plusplus.git:gh-pages

--- search/all_0.js
+++ search/all_0.js
@@ -3,7 +3,7 @@ var searchData=
   ['alignment_5f32_5fbit_0',['ALIGNMENT_32_BIT',['../_settings_8hpp.html#a4ad7a10e56a719ecc3b9e3fa1ce4bf12',1,'Settings.hpp']]],
   ['alignment_5f64_5fbit_1',['ALIGNMENT_64_BIT',['../_settings_8hpp.html#a3841f237ed7f4ea1fd5f6a42e4462f5c',1,'Settings.hpp']]],
   ['alpha_2',['Alpha',['../class_move_strategy_3_01_strategies_1_1_m_e_t_r_o_p_o_l_i_s_00_01_manifold_type_01_4.html#a61a8b1bcd14b978d38c43c32694ec88b',1,'MoveStrategy&lt; Strategies::METROPOLIS, ManifoldType &gt;']]],
-  ['apply_5fmove_3',['apply_move',['../_apply__move_8hpp.html#afa97b1c614f34a78339d573d1ecb408e',1,'Apply_move.hpp']]],
+  ['apply_5fmove_3',['apply_move',['../_apply__move_8hpp.html#afcd4bb134ff666e245b652a3f0dd0db2',1,'Apply_move.hpp']]],
   ['apply_5fmove_2ehpp_4',['Apply_move.hpp',['../_apply__move_8hpp.html',1,'']]],
   ['apply_5fmove_5ftest_2ecpp_5',['Apply_move_test.cpp',['../_apply__move__test_8cpp.html',1,'']]],
   ['are_5fall_5fvertex_5ftimevalues_5fvalid_6',['are_all_vertex_timevalues_valid',['../classmanifolds_1_1_manifold_3_013_01_4.html#ade4d42e231238623ca3e1aa1a6118758',1,'manifolds::Manifold&lt; 3 &gt;']]],
