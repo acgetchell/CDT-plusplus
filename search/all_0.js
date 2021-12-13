2a42cb6d68e4a597622c50c1c0c936bff2590036
Adam Getchell (via Travis CI) <acgetchell@ucdavis.edu>
2021-12-13 00:16:34 +0000

    Deploy acgetchell/CDT-plusplus to github.com/acgetchell/CDT-plusplus.git:gh-pages

--- search/all_0.js
+++ search/all_0.js
@@ -3,7 +3,7 @@ var searchData=
   ['alignment_5f32_5fbit_0',['ALIGNMENT_32_BIT',['../_settings_8hpp.html#a4ad7a10e56a719ecc3b9e3fa1ce4bf12',1,'Settings.hpp']]],
   ['alignment_5f64_5fbit_1',['ALIGNMENT_64_BIT',['../_settings_8hpp.html#a3841f237ed7f4ea1fd5f6a42e4462f5c',1,'Settings.hpp']]],
   ['alpha_2',['Alpha',['../class_move_strategy_3_01_strategies_1_1_m_e_t_r_o_p_o_l_i_s_00_01_manifold_type_01_4.html#a61a8b1bcd14b978d38c43c32694ec88b',1,'MoveStrategy&lt; Strategies::METROPOLIS, ManifoldType &gt;']]],
-  ['apply_5fmove_3',['apply_move',['../_apply__move_8hpp.html#afa97b1c614f34a78339d573d1ecb408e',1,'Apply_move.hpp']]],
+  ['apply_5fmove_3',['apply_move',['../_apply__move_8hpp.html#a2dedddbaf919af2f5535ccad6f1946b3',1,'Apply_move.hpp']]],
   ['apply_5fmove_2ehpp_4',['Apply_move.hpp',['../_apply__move_8hpp.html',1,'']]],
   ['apply_5fmove_5ftest_2ecpp_5',['Apply_move_test.cpp',['../_apply__move__test_8cpp.html',1,'']]],
   ['as_5finteger_6',['as_integer',['../_move__tracker_8hpp.html#a532ba88e802845b59d7e7adddc217c61',1,'move_tracker']]]
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['alpha_325',['Alpha',['../class_move_strategy_3_01_strategies_1_1_m_e_t_r_o_p_o_l_i_s_00_01_manifold_type_01_4.html#a61a8b1bcd14b978d38c43c32694ec88b',1,'MoveStrategy&lt; Strategies::METROPOLIS, ManifoldType &gt;']]],
-  ['apply_5fmove_326',['apply_move',['../_apply__move_8hpp.html#afa97b1c614f34a78339d573d1ecb408e',1,'Apply_move.hpp']]],
+  ['apply_5fmove_326',['apply_move',['../_apply__move_8hpp.html#a2dedddbaf919af2f5535ccad6f1946b3',1,'Apply_move.hpp']]],
   ['as_5finteger_327',['as_integer',['../_move__tracker_8hpp.html#a532ba88e802845b59d7e7adddc217c61',1,'move_tracker']]]
 ];
