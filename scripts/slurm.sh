ebab69095578f3dcf378a3448f695c409c852f3f
Adam Getchell <acgetchell@ucdavis.edu>
2022-10-13 19:34:06 -0700

    (build) Update Slurm script for HPC runs
    Also update packages to vcpkgGitCommitId: 929fc61671bb5e5e7322981dce1153abf24f54cf
    
    And re-enable Windows builds on GHA.

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -l
 # For use on Slurm
 # sbatch -p high -t 60 slurm.sh
-module load cmake/3.18.0
-module load spack/gcc/10.3.0
-module load spack/autoconf-archive/2019.01.06
+module load spack/cmake
+module load spack/gcc
+module load spack/autoconf-archive
 cd ..
 rm -rf build/
-cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -S . -B build
+cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo -D ENABLE_TESTING:BOOL=TRUE -S . -B build
 cmake --build build
+cd build || exit
+ctest --output-on-failure -j2
 #mkdir "$HOME"/data/"$(date +%Y-%m-%d.%R%Z)"
 #cp cdt-opt "$HOME"/data/"$(date +%Y-%m-%d.%R%Z)"/
 #cd "$HOME"/data/"$(date +%Y-%m-%d.%R%Z)" || exit
