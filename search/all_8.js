28b2642265e1d4d4daee77ebe45bd52e1b0382d1
Adam Getchell (via Travis CI) <acgetchell@ucdavis.edu>
2023-11-06 19:05:22 +0000

    Deploy acgetchell/CDT-plusplus to github.com/acgetchell/CDT-plusplus.git:gh-pages

--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['k_144',['K',['../class_move_strategy_3_01_strategies_1_1_m_e_t_r_o_p_o_l_i_s_00_01_manifold_type_01_4.html#ad5cfa55c2c5d547911bdb5199b9a324f',1,'MoveStrategy&lt; Strategies::METROPOLIS, ManifoldType &gt;']]],
-  ['kd_145',['Kd',['../_periodic__3__triangulations_8hpp.html#a47758eef4587eeb8e5419c87a5cd9943',1,'Periodic_3_triangulations.hpp']]]
+  ['k_143',['K',['../class_move_strategy_3_01_strategies_1_1_m_e_t_r_o_p_o_l_i_s_00_01_manifold_type_01_4.html#ad5cfa55c2c5d547911bdb5199b9a324f',1,'MoveStrategy&lt; Strategies::METROPOLIS, ManifoldType &gt;']]],
+  ['kd_144',['Kd',['../_periodic__3__triangulations_8hpp.html#a47758eef4587eeb8e5419c87a5cd9943',1,'Periodic_3_triangulations.hpp']]]
 ];
