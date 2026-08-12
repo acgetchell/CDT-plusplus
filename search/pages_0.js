e8c83932f0a353b326647d0d331ac0d65e297499
acgetchell <adam@adamgetchell.org>
2026-08-12 02:53:03 +0000

    Deploying to gh-pages from @ acgetchell/CDT-plusplus@b8e35a6c1accb0837a6d8cfa13eb3beb739c906a 🚀

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,14 +1,14 @@
 var searchData=
 [
-  ['0_200_202026_2008_2010_0',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc3...v1.0.0&quot;&gt;1.0.0&lt;/a&gt; - 2026-08-10',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100---2026-08-10',1,'']]],
+  ['0_200_202026_2008_2011_0',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc3...v1.0.0&quot;&gt;1.0.0&lt;/a&gt; - 2026-08-11',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100---2026-08-11',1,'']]],
   ['0_200_20rc1_202026_2007_2022_1',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/0.1.8...v1.0.0-rc1&quot;&gt;1.0.0-rc1&lt;/a&gt; - 2026-07-22',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc1---2026-07-22',1,'']]],
   ['0_200_20rc2_202026_2007_2022_2',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc1...v1.0.0-rc2&quot;&gt;1.0.0-rc2&lt;/a&gt; - 2026-07-22',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc2---2026-07-22',1,'']]],
   ['0_200_20rc3_202026_2007_2028_3',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc2...v1.0.0-rc3&quot;&gt;1.0.0-rc3&lt;/a&gt; - 2026-07-28',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc3---2026-07-28',1,'']]],
-  ['0_202026_2008_2010_4',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc3...v1.0.0&quot;&gt;1.0.0&lt;/a&gt; - 2026-08-10',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100---2026-08-10',1,'']]],
+  ['0_202026_2008_2011_4',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc3...v1.0.0&quot;&gt;1.0.0&lt;/a&gt; - 2026-08-11',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100---2026-08-11',1,'']]],
   ['0_20rc1_202026_2007_2022_5',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/0.1.8...v1.0.0-rc1&quot;&gt;1.0.0-rc1&lt;/a&gt; - 2026-07-22',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc1---2026-07-22',1,'']]],
   ['0_20rc2_202026_2007_2022_6',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc1...v1.0.0-rc2&quot;&gt;1.0.0-rc2&lt;/a&gt; - 2026-07-22',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc2---2026-07-22',1,'']]],
   ['0_20rc3_202026_2007_2028_7',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc2...v1.0.0-rc3&quot;&gt;1.0.0-rc3&lt;/a&gt; - 2026-07-28',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc3---2026-07-28',1,'']]],
   ['07_2022_8',['07 22',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc1---2026-07-22',1,'&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/0.1.8...v1.0.0-rc1&quot;&gt;1.0.0-rc1&lt;/a&gt; - 2026-07-22'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc2---2026-07-22',1,'&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc1...v1.0.0-rc2&quot;&gt;1.0.0-rc2&lt;/a&gt; - 2026-07-22']]],
   ['07_2028_9',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc2...v1.0.0-rc3&quot;&gt;1.0.0-rc3&lt;/a&gt; - 2026-07-28',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100-rc3---2026-07-28',1,'']]],
-  ['08_2010_10',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc3...v1.0.0&quot;&gt;1.0.0&lt;/a&gt; - 2026-08-10',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100---2026-08-10',1,'']]]
+  ['08_2011_10',['&lt;a href=&quot;https://github.com/acgetchell/CDT-plusplus/compare/v1.0.0-rc3...v1.0.0&quot;&gt;1.0.0&lt;/a&gt; - 2026-08-11',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md100---2026-08-11',1,'']]]
 ];
